c85ea17dd098e3f7efc6b7b013f8338529a6b4da
syoch <hacksyoch@gmail.com>
2020-12-07 00:06:13 +0900

    implement set/debug test

--- test/parsetest.cpp
+++ test/parsetest.cpp
@@ -1,6 +1,9 @@
 #include <parser.h>
+using namespace std::literals::string_literals;
 int main()
 {
-    
+    parser prs;
+    prs.set(L"func sendMessagesToPlayers()\n{\n  for player in ServerPlayer.List.name {\n    flag = [[0x10000000] + 0x10] + 0x100;\n    (*flag)++;\n    player.Chat << f\"{player.name}さん、こんにちは！\";\n}"s);
+    prs.debug();
     return 0;
 }
