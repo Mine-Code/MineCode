1c3767bbfcd8606b004a2ede94f9e327aa1917b7
syoch <hacksyoch@gmail.com>
2020-12-07 07:12:50 +0900

    Add prototype

---
+++ source/include/chariterator.h
@@ -0,0 +1,22 @@
+#pragma once
+#ifndef CHITER_H
+#define CHITER_H
+
+#include <string>
+#include <vector>
+
+class chariterator
+{
+private:
+    std::wstring str;
+    int index;
+public:
+    chariterator(std::wstring);
+    wchar_t next();
+    wchar_t prev();
+
+    wchar_t peek();
+    bool hasData();
+};
+
+#endif
