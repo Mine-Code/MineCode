02897fea16d5cb9fb0f8cb4c33c12383e2d2b08f
syoch <hacksyoch@gmail.com>
2020-12-07 00:12:51 +0900

    update memo(int -> dec,oct,hex)

--- source/src/parser.cpp
+++ source/src/parser.cpp
@@ -16,7 +16,7 @@ void parser::set(std::wstring src){
     string=src;
 }
 void parser::tokenize(){
-    //TODO: tokenize for word,int
+    //TODO: tokenize for word,oct,dec,hex
     size_t progsize=string.length();
     wchar_t ch;
     for (size_t i = 0; i < progsize; i++)
