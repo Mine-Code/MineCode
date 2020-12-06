62b9cc2df290749d21e59ceff8a39121099036e5
syoch <hacksyoch@gmail.com>
2020-12-07 07:09:59 +0900

    Add oct/hex branch

--- source/src/parser.cpp
+++ source/src/parser.cpp
@@ -16,12 +16,21 @@ void parser::set(std::wstring src){
     string=src;
 }
 void parser::tokenize(){
-    //TODO: tokenize for word,oct,dec,hex
+    //TODO: tokenize for word,dec
     size_t progsize=string.length();
     wchar_t ch;
     for (size_t i = 0; i < progsize; i++)
     {
         ch=string[i];
+        // oct/hex
+        if(ch=='0'){
+            if(string[i+1]=='o'){
+                // TODO: tokenize oct
+            }
+            if(string[i+1]=='x'){
+                // TODO: tokenize hex
+            }
+        }
     }
     
 }
