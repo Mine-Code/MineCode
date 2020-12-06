0f6bb3169d99f8e6fa0406d2df23506b6789804e
syoch <hacksyoch@gmail.com>
2020-12-07 07:07:03 +0900

    Fix test grammer

--- test/parsetest.cpp
+++ test/parsetest.cpp
@@ -3,7 +3,7 @@ using namespace std::literals::string_literals;
 int main()
 {
     parser prs;
-    prs.set(L"func sendMessagesToPlayers()\n{\n  for player in ServerPlayer.List.name {\n    flag = [[0x10000000] + 0o20] + 0x100;\n    (*flag)++;\n    player.Chat << f\"{player.name}さん、こんにちは！\";\n  }\n}"s);
+    prs.set(L"func sendMessagesToPlayers()\n{\n  for player in ServerPlayer.List.name {\n    flag = [[0x10000000] + 0o20] + 0x100;\n    [flag]++;\n    player.Chat << f\"{player.name}さん、こんにちは！\";\n  }\n}"s);
     prs.tokenize();
     prs.debug();
     return 0;
