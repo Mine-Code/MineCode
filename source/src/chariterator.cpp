c0b4a5e16652a13a742f6051d9808decf348ce1f
syoch <hacksyoch@gmail.com>
2020-12-07 23:07:43 +0900

    implement next

--- source/src/chariterator.cpp
+++ source/src/chariterator.cpp
@@ -1,5 +1,15 @@
 #include <chariterator.h>
+#include <iostream>
 
 chariterator::chariterator(std::wstring src)
-    :str(src)
+    :str(src), index(0)
 {}
+
+wchar_t chariterator::next(){
+    if(str.length() <= index+1){
+        std::cerr << "Out of size" << std::endl;;
+        throw L"outofsize";
+    }
+    index++;
+    return str[index];
+}
