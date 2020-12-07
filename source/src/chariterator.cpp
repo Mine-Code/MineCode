c2b470c3a10ec400e7422340dfa7d06c56bf7413
syoch <hacksyoch@gmail.com>
2020-12-08 07:27:35 +0900

    implement offseted peek

--- source/src/chariterator.cpp
+++ source/src/chariterator.cpp
@@ -25,6 +25,14 @@ wchar_t chariterator::peek(){
     return str[index];
 }
 
+wchar_t chariterator::peek(size_t offset){
+    if(index <= 0 || str.length() <= index){
+        std::cerr << "Out of size" << std::endl;;
+        throw L"outofsize";
+    }
+    return str[index+offset];
+}
+
 bool chariterator::hasData(){
     if(str.length() <= index){
         return false;
