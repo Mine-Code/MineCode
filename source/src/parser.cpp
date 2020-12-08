902a3d2ab1c838de6a28eee63b450bae0e4a5504
syoch <hacksyoch@gmail.com>
2020-12-08 20:08:46 +0900

    fix comment

--- source/src/parser.cpp
+++ source/src/parser.cpp
@@ -29,7 +29,7 @@ void parser::tokenize(){
             {
             case 'x':
                 //hex
-                chiter.next(); // skip '0'
+                chiter.next(); // skip 'x'
                 while(1){
                     ch=chiter.peek();
                     if(!chiter.hasData()){
