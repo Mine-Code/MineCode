d8860a81d66931db41cbd984a5631ba1b78f0377
syoch <hacksyoch@gmail.com>
2020-12-14 17:41:20 +0900

    add '#do once' support

--- source/src/parserWrap.cpp
+++ source/src/parserWrap.cpp
@@ -1,7 +1,11 @@
 #include <parserWrap.h>
-
+#include <assert.h>
 namespace parserWrap{
     void program(streamType& st,tokensType tokens){
-        
+        if(tokens.peek()==L"#"){
+            assert(tokens.next() == L"#"   );
+            assert(tokens.next() == L"do"  );
+            assert(tokens.next() == L"once");
+        }
     }
 }
