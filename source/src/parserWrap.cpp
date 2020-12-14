920280017d76a38289c4b3dc02104c2c0688ddc0
syoch <hacksyoch@gmail.com>
2020-12-15 07:31:27 +0900

    implement stmt (many todo)

--- source/src/parserWrap.cpp
+++ source/src/parserWrap.cpp
@@ -3,7 +3,7 @@
 #include <assert.h>
 
 #include <parserContext.h>
-
+#include <util.h>
 
 namespace parserWrap{
     void program(parserCtx::parserContext& ctx){
@@ -15,6 +15,33 @@ namespace parserWrap{
         }
     }
     void stmt(parserCtx::parserContext& ctx){
-        
+        // stmt Switcher
+        std::wstring text=ctx.iter.peek();
+        if(text==L"for"){
+            // TODO:call 'for'
+        }else if(text==L"while"){
+            // TODO:call 'while'
+        }else if(text==L"if"){
+            // TODO:call 'if'
+        }else if(text==L"func"){
+            // TODO:call 'func'
+        }else if(text==L"for"){
+            // TODO:call 'for'
+        }else{
+            //put / assign
+            assert(ctx.iter.hasData());
+            ctx.iter.next();
+            text=ctx.iter.peek();
+
+            if(text==L"<<"){
+                // TODO:call 'put'
+            }else if(
+                   text==L"++"
+                || text==L"--"
+                || util::isAssignOp(text)
+            ){
+                // TODO:call 'assign'
+            }
+        }
     }
 }
