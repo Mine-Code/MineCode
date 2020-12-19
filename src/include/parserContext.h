56a0239c370a39748f60052b9d9bf00430baf573
syoch <hacksyoch@gmail.com>
2020-12-19 23:54:44 +0900

    update: change stream type in parserctx

--- src/include/parserContext.h
+++ src/include/parserContext.h
@@ -5,19 +5,18 @@
 #include <unordered_map>
 #include <sstream>
 #include "typedIterator.hxx"
-
+#include "asm.h"
 namespace parserCtx
 {
     enum varType{
         FLOAT,
         INT
     };
-    using streamType = std::wstringstream;
     using iterType   = iterator<std::wstring>;
     using varsType   = std::unordered_map<std::string,varType>;
     struct parserContext{
         parserContext();
-        streamType stream;
+        Assembly* Asm;
         iterType iter;
         varsType variables;
     };
