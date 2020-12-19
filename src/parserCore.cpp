c72ec6ad97011124efef6a52be1779a3a0f942e7
syoch <hacksyoch@gmail.com>
2020-12-19 18:30:27 +0900

    update: condition in parserCore::value@L104

--- src/parserCore.cpp
+++ src/parserCore.cpp
@@ -101,16 +101,14 @@ namespace parserCore{
     }
     std::wstring value(parserCtx::parserContext& ctx){
         wchar_t ch=ctx.iter.peek()[0];
-        if(ctx.iter.peek()==L"["){
+        if(ctx.iter.peek(1)==L"..."){
+            return range(ctx);
+        }else if(ctx.iter.peek()==L"["){
             return ptr(ctx);
         }else if(ctx.iter.peek(1)==L"."){
             return attribute(ctx);
         }else if(isdigit(ch)){
-            if(ctx.iter.peek(2)==L"..."){
-                return range(ctx);
-            }else{
-                return ctx.iter.next();
-            }
+            return ctx.iter.next();
         }else if(ch==L'"'){
             return ctx.iter.next();
         }else{
