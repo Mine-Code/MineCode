17b7b8dba45db4d802353a0a21ab9db23bed1d86
Yuki <yukikamome316@gmail.com>
2020-12-20 01:01:47 +0900

    update: fix some functions which go wrong

--- test/AssemblyTest/main.cpp
+++ test/AssemblyTest/main.cpp
@@ -6,7 +6,7 @@ int main()
     std::wstringstream ss;
     Assembly assembly(ss);
     assembly.startOfFunction();
-    assembly.callFunction(0x10000000);
+    assembly.callFunction(0xABCDEF01);
     assembly.endOfFunction();
     std::wcout << ss.str() << std::endl;
 }
