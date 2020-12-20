148ecd66d489348afdb3c4dc7f1efe4a37601676
syoch <hacksyoch@gmail.com>
2020-12-20 18:25:47 +0900

    update: using reference(parserCtx)

--- src/stmtProcessor.cpp
+++ src/stmtProcessor.cpp
@@ -2,33 +2,33 @@
 #include <parserContext.h>
 #include <parserCore.h>
 
-void For    (parserCtx::parserContext ctx){
+void For    (parserCtx::parserContext& ctx){
     while(ctx.iter.hasData()){
         if(ctx.iter.peek()==L"}")break;
         parserCore::stmt(ctx);
     }
 }
 
-void Forr   (parserCtx::parserContext ctx){
+void Forr   (parserCtx::parserContext& ctx){
 
 }
 
-void While  (parserCtx::parserContext ctx){
+void While  (parserCtx::parserContext& ctx){
 
 }
 
-void If     (parserCtx::parserContext ctx){
+void If     (parserCtx::parserContext& ctx){
 
 }
 
-void Func   (parserCtx::parserContext ctx){
+void Func   (parserCtx::parserContext& ctx){
 
 }
 
-void Put    (parserCtx::parserContext ctx){
+void Put    (parserCtx::parserContext& ctx){
 
 }
 
-void Assign (parserCtx::parserContext ctx){
+void Assign (parserCtx::parserContext& ctx){
 
 }
