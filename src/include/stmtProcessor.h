d1b2788eccab5195e1596041ad2fb412d9195582
syoch <hacksyoch@gmail.com>
2020-12-20 10:01:33 +0900

    add: define stmt process functions

--- src/include/stmtProcessor.h
+++ src/include/stmtProcessor.h
@@ -2,9 +2,19 @@
 #ifndef STMT_PROC_H
 #define STMT_PROC_H
 
+namespace parserCtx
+{
+    class parserContext;
+} // namespace parserCtx
+
 namespace stmtProcessor
 {
-    
+    void For    (parserCtx::parserContext ctx);
+    void While  (parserCtx::parserContext ctx);
+    void If     (parserCtx::parserContext ctx);
+    void Func   (parserCtx::parserContext ctx);
+    void Put    (parserCtx::parserContext ctx);
+    void Assign (parserCtx::parserContext ctx);
 } // namespace stmtProcessor
 
 
---
+++ src/stmtProcessor.cpp
@@ -0,0 +1,21 @@
+#include <stmtProcessor.h>
+#include <parserContext.h>
+
+void For    (parserCtx::parserContext ctx){
+    
+}
+void While  (parserCtx::parserContext ctx){
+    
+}
+void If     (parserCtx::parserContext ctx){
+    
+}
+void Func   (parserCtx::parserContext ctx){
+    
+}
+void Put    (parserCtx::parserContext ctx){
+    
+}
+void Assign (parserCtx::parserContext ctx){
+    
+}
