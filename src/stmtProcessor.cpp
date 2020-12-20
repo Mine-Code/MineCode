93ab54a3aabc68b7a0d2c21788501b2070233d2a
syoch <hacksyoch@gmail.com>
2020-12-20 11:07:37 +0900

    update: little format

--- src/stmtProcessor.cpp
+++ src/stmtProcessor.cpp
@@ -2,20 +2,25 @@
 #include <parserContext.h>
 
 void For    (parserCtx::parserContext ctx){
-    
+
 }
+
 void While  (parserCtx::parserContext ctx){
-    
+
 }
+
 void If     (parserCtx::parserContext ctx){
-    
+
 }
+
 void Func   (parserCtx::parserContext ctx){
-    
+
 }
+
 void Put    (parserCtx::parserContext ctx){
-    
+
 }
+
 void Assign (parserCtx::parserContext ctx){
-    
+
 }
