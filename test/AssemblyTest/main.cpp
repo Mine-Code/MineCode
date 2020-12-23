57b8602b342be59be2b4619dbcd66d09fa376046
Yuki <yukikamome316@gmail.com>
2020-12-23 19:43:56 +0900

    update: change the order of arguments in some functions

--- test/AssemblyTest/main.cpp
+++ test/AssemblyTest/main.cpp
@@ -3,14 +3,16 @@
 
 int main()
 {
-    //FreeFly [[0x109CD8E4] + 0x34] + 0x70C = 0x01000100
     std::wstringstream ss;
     Assembly assembly(ss);
     assembly.startOfFunction();
-    assembly.peek(0x109CD8E4);
-    assembly.peek(0, true, 0x34, 13, 12);
+
+    //FreeFly [[0x109CD8E4] + 0x34] + 0x70C = 0x01000100
+    assembly.peek_i(0x109CD8E4);
+    assembly.peek(0x34, 12);
     assembly.writeResister(0x01000101, 13);
-    assembly.poke(0, false, 0x70C);
+    assembly.poke(0x70C);
+
     assembly.endOfFunction();
     std::wcout << ss.str() << std::endl;
 }
