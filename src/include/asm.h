26eb20e0614e62edc5131997f90c7cf888d4ff60
Yuki <yukikamome316@gmail.com>
2020-12-23 17:32:30 +0900

    update: support peek with pointer

--- src/include/asm.h
+++ src/include/asm.h
@@ -12,9 +12,13 @@ public:
     Assembly(std::wstringstream& ss);
     void startOfFunction();
     void endOfFunction();
+
+    void writeResister(int value, int dest);
+    void moveResister(int src, int dest);
+
     void callFunction(int address);
-    void peek(int address, int dest = 13);
-    void poke(int address, int src = 13);
+    void peek(int address = 0, bool is_ptr = false, int offset = 0, int src = 13, int dest = 13);
+    void poke(int address = 0, bool is_immediate = true, int offset = 0, int dest = 12, int src = 13);
     void add(int value, int src = 13, int dest = 13);
     void mul(int value, int src = 13, int dest = 13);
     void div(int value, int src = 13, int dest = 13);
--- test/AssemblyTest/main.cpp
+++ test/AssemblyTest/main.cpp
@@ -3,12 +3,14 @@
 
 int main()
 {
+    //FreeFly [[0x109CD8E4] + 0x34] + 0x70C = 0x01000100
     std::wstringstream ss;
     Assembly assembly(ss);
     assembly.startOfFunction();
-    ss << "li r13, 2\n";
-    assembly.mul(2);
-    assembly.poke(0x10000000);
+    assembly.peek(0x109CD8E4);
+    assembly.peek(0, true, 0x34, 13, 12);
+    assembly.writeResister(0x01000101, 13);
+    assembly.poke(0, false, 0x70C);
     assembly.endOfFunction();
     std::wcout << ss.str() << std::endl;
 }
