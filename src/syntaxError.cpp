90dea5988d36aebd0f2c79df2b67c66b8038b3c0
syoch <hacksyoch@gmail.com>
2020-12-23 19:15:28 +0900

    update: implement stringize of reportError

--- src/syntaxError.cpp
+++ src/syntaxError.cpp
@@ -3,4 +3,11 @@
 
 using namespace synErr;
 
-void synErr::ReportError(Context ctx,errorType type){}
+void synErr::ReportError(Context ctx,errorType type){
+    std::wstring strType;
+    if(type==SYNTAX){
+        strType=L"syntax";
+    }else if(type==TOKENIZE){
+        strType=L"tokenize";
+    }
+}
