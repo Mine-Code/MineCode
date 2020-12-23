e5651502a75618e555811e08a901de2a28b14ac3
syoch <hacksyoch@gmail.com>
2020-12-23 19:20:08 +0900

    update: implement ReportError

--- src/syntaxError.cpp
+++ src/syntaxError.cpp
@@ -10,4 +10,17 @@ void synErr::ReportError(Context ctx,errorType type,std::wstring exMsg){
     }else if(type==TOKENIZE){
         strType=L"tokenize";
     }
+
+    std::wcout
+        << "-------Error--------"<<std::endl
+        << "| type:"<<strType<<std::endl;
+    if(exMsg!=L"")std::wcout
+        << "|  msg:"<<exMsg<<std::endl;
+    std::wcout
+        << "| Debug Informations"
+        << "|  peek(-1):"<<ctx.iter.peek(-1)<<std::endl
+        << "|  peek( 0):"<<ctx.iter.peek( 0)<<std::endl
+        << "|  peek(+1):"<<ctx.iter.peek(+1)<<std::endl
+        << "--------------------"<<std::endl;
+    
 }
