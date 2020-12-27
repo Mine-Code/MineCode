875b0b629495fa10bd78cd0940d72752a17c3b5b
syoch <hacksyoch@gmail.com>
2020-12-27 15:00:59 +0900

    update: add to int

--- src/include/util.h
+++ src/include/util.h
@@ -120,6 +120,34 @@ namespace util
             || isCondOp(val)
             || isMathOp(val);
     }
+
+    template<typename T>
+    int toInt(T text){
+        int value=0;
+        if(text[0]==L'0' && (text[1]==L'x' || text[1]==L'o') ){
+            if(text[1]==L'x'){
+                for(auto ch: text.substr(2)){
+                    value*=0x10;
+                    if(util::inRange(L'0',ch,L'9')){
+                        value+=ch-'0';
+                    }else{
+                        value+=tolower(ch)-'a';
+                    }
+                }
+            }else if(text[1]==L'o'){
+                for(auto ch: text.substr(2)){
+                    value*=8;
+                    value+=ch-'0';
+                }
+            }
+        }else{
+            for(auto ch: text){
+                value*=10;
+                value+=ch-'0';
+            }
+        }
+        return value;
+    }
 } // namespace util
 
 
