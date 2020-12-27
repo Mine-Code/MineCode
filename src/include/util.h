6fa33cd2df1c5c82c9c38ded163da389e99dc300
syoch <hacksyoch@gmail.com>
2020-12-27 15:12:08 +0900

    refactor: simplize toInt by _toInt

--- src/include/util.h
+++ src/include/util.h
@@ -123,40 +123,7 @@ namespace util
 
     template<typename T>
     int toInt(T _text){
-        //preprocessing _text
-        T text;
-        int sign;
-        if(_text[0]=='+' || _text[0]=='-'){
-            text=_text.substr(1);
-            sign=+1;
-        }else{
-            text=_text;
-            sign=-1;
-        }
-        int value=0;
-        if(text[0]==L'0' && (text[1]==L'x' || text[1]==L'o') ){
-            if(text[1]==L'x'){
-                for(auto ch: text.substr(2)){
-                    value*=0x10;
-                    if(util::inRange(L'0',ch,L'9')){
-                        value+=ch-'0';
-                    }else{
-                        value+=tolower(ch)-'a';
-                    }
-                }
-            }else if(text[1]==L'o'){
-                for(auto ch: text.substr(2)){
-                    value*=8;
-                    value+=ch-'0';
-                }
-            }
-        }else{
-            for(auto ch: text){
-                value*=10;
-                value+=ch-'0';
-            }
-        }
-        return value*sign;
+        return _toInt(std::wstring(_text));
     }
 } // namespace util
 
