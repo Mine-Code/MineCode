ae7a4878098957e190dc3c23beb7097289eace20
syoch <hacksyoch@gmail.com>
2020-12-27 14:34:53 +0900

    update: add argument to Forr (stmtproc)

--- src/include/stmtProcessor.h
+++ src/include/stmtProcessor.h
@@ -15,7 +15,7 @@ namespace stmtProcessor
     using String = std::wstring;
 
     void For    (parserCtx::parserContext& ctx,String,String);
-    void Forr   (parserCtx::parserContext& ctx);
+    void Forr   (parserCtx::parserContext& ctx,int,int);
     void While  (parserCtx::parserContext& ctx);
     void If     (parserCtx::parserContext& ctx);
     void Func   (parserCtx::parserContext& ctx);
