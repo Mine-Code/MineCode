b5fe9805694ed2f4b9fda2e0ad6f5dd464b2843f
syoch <hacksyoch@gmail.com>
2020-12-27 15:18:36 +0900

    update: add support of optimizition of expr

--- src/parserCore.cpp
+++ src/parserCore.cpp
@@ -259,12 +259,13 @@ namespace parserCore{
         std::wstring ret;
         int imm;
         for(auto part:parts){
-            if(isdigit(part[0])){
-                //TODO add imm += part
+            if(isdigit(part[1])){
+                imm+=util::toInt(part);
+            }else{
+                ret+=part;
             }
-            ret+=part;
         }
-        return ret;
+        return std::to_wstring(imm)+L"+"+ret;
     }
     Range range  (parserCtx::parserContext& ctx){
         int start=Int(ctx);
