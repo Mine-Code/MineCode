3da6c34bea60d59011584fd3659cfa73b6b8d5fd
syoch <hacksyoch@gmail.com>
2020-12-31 00:31:59 +0900

    update: implement assert_fail

--- src/myassert.cpp
+++ src/myassert.cpp
@@ -6,5 +6,5 @@
 #include <parserTypes.h>
 
 void assert_fail(const char* fname,const char* funcname,int line,parserTypes::parserContext& ctx,wchar_t* msg){
-    
+    synErr::syntaxError(ctx,msg,fname,funcname,line);
 }
