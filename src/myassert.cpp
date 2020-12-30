68d9f2719d347090a29aa3c73a1632914a5f097d
syoch <hacksyoch@gmail.com>
2020-12-31 00:21:40 +0900

    fix: add parserTypes.h depend

--- src/myassert.cpp
+++ src/myassert.cpp
@@ -3,7 +3,8 @@
 #include <sstream>
 
 #include <syntaxError.h>
+#include <parserTypes.h>
 
-void assert_fail(const char* fname,const char* funcname,int line,wchar_t* msg){
+void assert_fail(const char* fname,const char* funcname,int line,parserTypes::parserContext,wchar_t* msg){
     
 }
