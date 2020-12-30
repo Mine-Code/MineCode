a08ac52181b80446052f93e20e0f4306ecaa740e
syoch <hacksyoch@gmail.com>
2020-12-30 23:56:01 +0900

    update: change types of where

--- src/include/syntaxError.h
+++ src/include/syntaxError.h
@@ -17,11 +17,11 @@ namespace synErr
         PROCESS
     };
     using Context=parserTypes::parserContext&;
-    void ReportError(Context,errorType,std::wstring,std::wstring,std::wstring,int);
+    void ReportError(Context,errorType,std::wstring,const char*,const char*,int);
 
-    void syntaxError(Context,std::wstring,std::wstring,std::wstring,int);
-    void tokenizeError(Context,std::wstring,std::wstring,std::wstring,int);
-    void processError(Context,std::wstring,std::wstring,std::wstring,int);
+    void   syntaxError(Context,std::wstring,const char*,const char*,int);
+    void tokenizeError(Context,std::wstring,const char*,const char*,int);
+    void  processError(Context,std::wstring,const char*,const char*,int);
 } // namespace synErr
 
 
