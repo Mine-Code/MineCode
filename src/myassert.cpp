501b6d6450bac21ef79e3fc7439d539563840075
syoch <hacksyoch@gmail.com>
2020-12-31 07:09:54 +0900

    fix: fix warning of constant

--- src/myassert.cpp
+++ src/myassert.cpp
@@ -5,6 +5,6 @@
 #include <syntaxError.h>
 #include <parserTypes.h>
 
-void assert_fail(const char* fname,const char* funcname,int line,parserTypes::parserContext& ctx,wchar_t* msg){
+void assert_fail(const char* fname,const char* funcname,int line,parserTypes::parserContext& ctx,const wchar_t* msg){
     synErr::syntaxError(ctx,msg,fname,funcname,line);
 }
