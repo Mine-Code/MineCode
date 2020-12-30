155c324512378940c0fc389d062b23773da0ed83
syoch <hacksyoch@gmail.com>
2020-12-31 08:13:26 +0900

    feat: add assertChar fucntion

--- src/include/myassert.h
+++ src/include/myassert.h
@@ -3,7 +3,6 @@
 #define MYASSERT_H
 
 #include <string>
-
 namespace parserTypes
 {
     class parserContext;
@@ -12,6 +11,8 @@ namespace parserTypes
 
 #define assert(cond,msg)\
     if(!(cond))assert_fail(__FILE__,__func__,__LINE__,ctx,msg)
+
+#define assertChar(ch) assert(ctx.iter.next()==L##ch,L"excepted '" ch "'");
 
 void assert_fail(const char*,const char*,int,parserTypes::parserContext&,const wchar_t*);
 
