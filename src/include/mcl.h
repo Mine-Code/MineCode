7767511ad36822396d5b169895725a67d9e6e75f
syoch <hacksyoch@gmail.com>
2021-01-01 09:14:21 +0900

    update: add mcl class

--- src/include/mcl.h
+++ src/include/mcl.h
@@ -2,4 +2,15 @@
 #ifndef MCL_H
 #define MCL_H
 
+#include <string>
+#include <../../lib/json/single_include/nlohmann/json.hpp>
+using json = nlohmann::json;
+
+class mcl{
+private:
+    json raw;
+public:
+    mcl(std::wstring name);
+};
+
 #endif
