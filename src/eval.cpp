9986cbaabfa8eb3f0a82babd12cb4ff3c2437ae2
syoch <hacksyoch@gmail.com>
2021-01-08 08:07:38 +0900

    update: add newExpr

--- src/eval.cpp
+++ src/eval.cpp
@@ -6,10 +6,13 @@ using namespace parserTypes;
 
 expr& optimize(expr& val){
     int immutable=0;
+    expr newExpr;
     for(auto part:val.parts){
         if(part.isSingle() && part.parts[0].isSingle() && part.parts[0].parts[0].type==power::IMM){
             // single pattern
             immutable+=part.parts[0].parts[0].imm;
+        }else{
+            newExpr.parts.emplace_back(part);
         }
     }
     return val;
