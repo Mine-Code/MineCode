ae2fa733f01296d84a317c1f8c4ebcdc1fc87365
syoch <hacksyoch@gmail.com>
2021-01-07 16:55:22 +0900

    add: prototypes of evalute functions

--- src/eval.cpp
+++ src/eval.cpp
@@ -1,3 +1,13 @@
 #include <eval.h>
 
 using namespace parserTypes;
+
+void eval::Expr (expr  val,int dest=13){
+
+}
+void eval::Expo (expo  val,int dest=13){
+
+}
+void eval::Power(power val,int dest=13){
+
+}
