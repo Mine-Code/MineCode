b790bbf81161f06eed687a94f1e9478a16a37824
syoch <hacksyoch@gmail.com>
2021-01-08 08:19:18 +0900

    update: rewrites ...

--- src/eval.cpp
+++ src/eval.cpp
@@ -5,25 +5,16 @@
 using namespace parserTypes;
 
 term& optimize(term& val){
-    int immutable=0;
+    int immutable_mul=0;
     term newTerm;
-    for(auto part:val.parts){
+    // process of mul
+    for(auto part:val.parts_mul){
         if(part.isSingle() && part.parts[0].type==power::IMM){
             // single pattern
-            immutable*=part.parts[0].imm;
+            immutable_mul*=part.parts[0].imm;
         }else{
-            newTerm.parts.emplace_back(part);
+            newTerm.parts_mul.emplace_back(part);
         }
-    }
-    if(immutable!=0){
-        power value;
-        value.type=power::IMM;
-        value.imm=immutable;
-
-        expo exponent;
-        exponent.parts.emplace_back(value);
-
-        val.parts.emplace_back(exponent);
     }
     return val;
 }
