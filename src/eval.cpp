44c66da4e84acac5b7cadad5fc4a276bd7cbd77d
syoch <hacksyoch@gmail.com>
2021-01-08 08:12:05 +0900

    update: implement optimize of term

--- src/eval.cpp
+++ src/eval.cpp
@@ -4,6 +4,30 @@
 
 using namespace parserTypes;
 
+term& optimize(term& val){
+    int immutable=0;
+    term newTerm;
+    for(auto part:val.parts){
+        if(part.isSingle() && part.parts[0].type==power::IMM){
+            // single pattern
+            immutable*=part.parts[0].imm;
+        }else{
+            newTerm.parts.emplace_back(part);
+        }
+    }
+    if(immutable!=0){
+        power value;
+        value.type=power::IMM;
+        value.imm=immutable;
+
+        expo exponent;
+        exponent.parts.emplace_back(value);
+
+        val.parts.emplace_back(exponent);
+    }
+    return val;
+}
+
 expr& optimize(expr& val){
     int immutable=0;
     expr newExpr;
