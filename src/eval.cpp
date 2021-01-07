e8ef61005617353c71aed5e2295addf52855e306
syoch <hacksyoch@gmail.com>
2021-01-08 08:58:17 +0900

    refactor: condition

--- src/eval.cpp
+++ src/eval.cpp
@@ -25,25 +25,27 @@ expr& optimize(expr& val){
     int immutable_mod=0;
     expr newExpr;
     for(auto part:val.parts){
-        if(
-            part.isSingle() &&
-            part.parts_mul[0].isSingle() &&
-            part.parts_mul[0].parts[0].type==power::IMM
-        ){
-            immutable_mul+=part.parts_mul[0].parts[0].imm;
-        }else if(
-            part.isSingle() &&
-            part.parts_div[0].isSingle() &&
-            part.parts_div[0].parts[0].type==power::IMM
-        ){
-            immutable_div+=part.parts_div[0].parts[0].imm;
-        }else if(
-            part.isSingle() &&
-            part.parts_mod[0].isSingle() &&
-            part.parts_mod[0].parts[0].type==power::IMM
-        ){
-            immutable_mod+=part.parts_mod[0].parts[0].imm;
-        }else{
+        if(part.isSingle()){
+            if(
+                part.parts_mul[0].isSingle() &&
+                part.parts_mul[0].parts[0].type==power::IMM
+            ){
+                immutable_mul+=part.parts_mul[0].parts[0].imm;
+            }else if(
+                part.parts_div[0].isSingle() &&
+                part.parts_div[0].parts[0].type==power::IMM
+            ){
+                immutable_div+=part.parts_div[0].parts[0].imm;
+            }else if(
+                part.parts_mod[0].isSingle() &&
+                part.parts_mod[0].parts[0].type==power::IMM
+            ){
+                immutable_mod+=part.parts_mod[0].parts[0].imm;
+            }else{
+                newExpr.parts.emplace_back(part);
+            }
+        }
+        else{
             newExpr.parts.emplace_back(part);
         }
     }
