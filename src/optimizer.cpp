f4fd393149ccbc340626b9cd94e236414d89f01b
syoch <hacksyoch@gmail.com>
2021-01-10 08:33:03 +0900

    update: implement optimzier construct

--- src/optimizer.cpp
+++ src/optimizer.cpp
@@ -1,3 +1,12 @@
 #include <optimizer.h>
 
-optimizer::optimizer(std::wstring src){}
+optimizer::optimizer(std::wstring src){
+    std::wstring line;
+    for(auto ch: src){
+        line+=ch;
+        if(ch=='\n'){
+            codes.emplace_back(line);
+            line=L"";
+        }
+    }
+}
