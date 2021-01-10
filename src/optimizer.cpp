3bf3b1267fb8d7faf5c82135da03f6108ad1bc62
syoch <hacksyoch@gmail.com>
2021-01-10 09:06:39 +0900

    update: move Iasm declare

--- src/optimizer.cpp
+++ src/optimizer.cpp
@@ -1,11 +1,6 @@
 #include <optimizer.h>
 #include <typedIterator.hxx>
 
-class Iasm{
-public:
-    std::wstring name;
-};
-
 optimizer::optimizer(std::wstring src){
     std::wstring line;
     for(auto ch: src){
