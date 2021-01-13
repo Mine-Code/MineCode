cc3ad43b1bed2452287bfd95ce85ab969ba50e8a
syoch <hacksyoch@gmail.com>
2021-01-14 07:02:27 +0900

    fix: add header depend

--- src/condeval.cpp
+++ src/condeval.cpp
@@ -1,10 +1,12 @@
 #include <condeval.h>
 
+#include <parserTypes.h>
+
 using namespace condeval;
 
 void condeval::Cond(parserTypes::parserContext ctx, parserTypes::cond cond){
-    
+
 }
 void condeval::CondChild(parserTypes::parserContext ctx, parserTypes::condChild cond){
-    
+
 }
