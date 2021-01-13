265e16cc9e34a8e666fae16ffa49f3b5b441389f
syoch <hacksyoch@gmail.com>
2021-01-14 07:08:15 +0900

    update: process of cond.first

--- src/condeval.cpp
+++ src/condeval.cpp
@@ -5,7 +5,8 @@
 using namespace parserTypes;
 
 void condeval::Cond(parserContext ctx, cond cond){
-
+    CondChild(ctx,cond.first);
+    // TODO: process cond.conds
 }
 void condeval::CondChild(parserContext ctx, condChild cond){
 
