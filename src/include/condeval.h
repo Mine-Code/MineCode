d693f3aefcd36db3362b92589ce8a1af2a40adb8
syoch <hacksyoch@gmail.com>
2021-01-18 13:22:10 +0900

    update: add function of condAnd

--- src/include/condeval.h
+++ src/include/condeval.h
@@ -15,6 +15,7 @@ namespace parserTypes
 namespace condeval
 {
     void Cond(parserTypes::parserContext ctx, parserTypes::cond cond);
+    void CondAnd(parserTypes::parserContext ctx, parserTypes::condAnd cond, std::wstring target);
     void CondChild(parserTypes::parserContext ctx, parserTypes::condChild cond, std::wstring target);
 } // namespace condeval
 
