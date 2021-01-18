0388c5d6b4d846b7c9175eacc7f4348462563210
syoch <hacksyoch@gmail.com>
2021-01-18 20:08:23 +0900

    update: add get label

--- src/condeval.cpp
+++ src/condeval.cpp
@@ -10,6 +10,7 @@ using namespace parserTypes;
 void condeval::Cond(parserContext ctx, cond cond){
     //CondChild(ctx,cond.first, L"if_"+std::to_wstring(ctx.Asm->make_if_ctr)+L":");
     //ctx.Asm->startOfIf_footer();
+    std::wstring label = L"if_"+std::to_wstring(ctx.Asm->make_if_ctr)+L":";
     for(auto condChild: cond.conds){
         // TODO: process condChild
     }
