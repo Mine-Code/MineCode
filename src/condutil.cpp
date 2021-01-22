cf2d68450f05cf71c1de016e907c3f698760920c
syoch <hacksyoch@gmail.com>
2021-01-22 09:56:46 +0900

    update: move invertConditional to condutil

--- src/condutil.cpp
+++ src/condutil.cpp
@@ -1,1 +1,36 @@
 #include <condutil.h>
+
+#include <parserTypes.h>
+
+using namespace parserTypes;
+
+condChild util::invertConditional(condChild source){
+    condChild::Type old=source.op;
+    switch(old){
+    case condChild::SINGLE:
+        source.op=condChild::SINGLE_INV;
+        break;
+    case condChild::SINGLE_INV:
+        source.op=condChild::SINGLE;
+        break;
+    case condChild::EQU:
+        source.op=condChild::NEQ;
+        break;
+    case condChild::NEQ:
+        source.op=condChild::EQU;
+        break;
+    case condChild::LT:
+        source.op=condChild::GE;
+        break;
+    case condChild::GT:
+        source.op=condChild::LE;
+        break;
+    case condChild::GE:
+        source.op=condChild::LT;
+        break;
+    case condChild::LE:
+        source.op=condChild::GT;
+        break;
+    }
+    return source;
+}
--- src/include/condutil.h
+++ src/include/condutil.h
@@ -2,4 +2,18 @@
 #ifndef CONDUTIL_H
 #define CONDUTIL_H
 
+namespace parserTypes
+{
+    class parserContext;
+    class cond;
+    class condAnd;
+    class condChild;
+} // namespace parserTypes
+
+namespace util{
+    using namespace parserTypes;
+    
+    condChild invertConditional(condChild source);
+}
+
 #endif
