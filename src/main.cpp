5730e0617e062162b9ddcf2c315c0eb187e7e75f
syoch <hacksyoch@gmail.com>
2021-01-26 16:47:49 +0900

    update: add argument check

--- src/main.cpp
+++ src/main.cpp
@@ -10,5 +10,13 @@ int main(int argc, char *argv[])
 {
 	parserWrap prs;
 	std::ifstream file;
+
+	//argument check
+	if(argc!=2){
+		std::wcout
+			<<"Error: Invalid argument count"<<std::endl
+			<<"[Usage] "<<argv[0]<<" <Source filename>"<<std::endl;
+	}
+
 	return 0;
 }
