dee105e9da00b9945d989f4f8ae7eb5eff8f2704
syoch <hacksyoch@gmail.com>
2021-02-04 06:29:54 +0900

    update: add forward declare

--- src/include/parserTypes.h
+++ src/include/parserTypes.h
@@ -22,16 +22,7 @@ namespace parserTypes
     using streamType = std::wstringstream;
     using iterType   = iterator<std::wstring>;
     using varsType   = std::unordered_map<std::string,struct varType>;
-    enum funcArgType{
-        INT,
-        CSTR,
-        WSTR,
-        PTR
-    };
-    struct funcArg{
-        funcArgType type;
-        struct expr defaultValue;
-    };
+    struct function;
     struct parserContext{
         parserContext();
         parserContext(parserContext&);
