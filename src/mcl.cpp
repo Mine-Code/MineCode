259036a8144e2f44b0988b6959760c026d7df73f
syoch <hacksyoch@gmail.com>
2021-02-07 21:37:10 +0900

    update: add convert process

--- src/mcl.cpp
+++ src/mcl.cpp
@@ -53,6 +53,7 @@ void operator<<(parserWrap& ctx, std::string name){
         auto proc=obj["proc"].get<std::string>();
         if(type=="MineCode"){
             std::wstring source=util::str2wstr(proc);
+            source=convPut(source);
             ctx.ctx.puts[name]=util::wstr2str(compiler.compile(source));
         }else if(type=="asm"){
             ctx.ctx.puts[name]=proc;
