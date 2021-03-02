a7e9751c0543befb7b26bb94ceeccd0be771df57
syoch <hacksyoch@gmail.com>
2021-03-02 20:17:39 +0900

    update: add nul

--- src/asm.cpp
+++ src/asm.cpp
@@ -3,121 +3,115 @@
 #include <wchar.h>
 #include <asm.h>
 
-
-Assembly::Assembly(std::wstringstream& _ss)
+Assembly::Assembly(std::wstringstream &_ss)
 	: ss(_ss)
 {
 }
 
 void Assembly::startOfFunction()
 {
-	ss <<
-		"stwu r1, -" << stack_size << "(r1)\n"
-		"mflr r0\n"
-		"stw r0, " << stack_size + 4 << "(r1)\n"
-		"stw r13, " << stack_size - 4 << "(r1)\n";
+	ss << "stwu r1, -" << stack_size << "(r1)\n"
+										"mflr r0\n"
+										"stw r0, "
+	   << stack_size + 4 << "(r1)\n"
+							"stw r13, "
+	   << stack_size - 4 << "(r1)\n";
 }
 
 void Assembly::endOfFunction()
 {
-	ss <<
-		"lwz r0, " << stack_size + 4 << "(r1)\n"
-		"lwz r13, " << stack_size - 4 << "(r1)\n"
-		"mtlr r0\n"
-		"addi r1, r1, " << stack_size << "\n"
-		"blr";
+	ss << "lwz r0, " << stack_size + 4 << "(r1)\n"
+										  "lwz r13, "
+	   << stack_size - 4 << "(r1)\n"
+							"mtlr r0\n"
+							"addi r1, r1, "
+	   << stack_size << "\n"
+						"blr";
 }
 
 void Assembly::writeRegister(int value, int dest)
 {
 	assert((unsigned int)value <= 0xFFFFFFFF);
 
-	int high = ((uint16_t*)&value)[1];
-	int low = ((uint16_t*)&value)[0];
-	if (low != 0) {}
-	if (high != 0) {
-		ss <<
-			"lis r" << dest << ", " << ((uint16_t*)&value)[1] << "\n";
-		if (low != 0)ss << "ori r" << dest << ", r" << dest << ", " << ((uint16_t*)&value)[0] << "\n";
-	} else {
-		ss <<
-			"li r" << dest << ", " << ((uint16_t*)&value)[0] << "\n";
+	int high = ((uint16_t *)&value)[1];
+	int low = ((uint16_t *)&value)[0];
+	if (low != 0)
+	{
+	}
+	if (high != 0)
+	{
+		ss << "lis r" << dest << ", " << ((uint16_t *)&value)[1] << "\n";
+		if (low != 0)
+			ss << "ori r" << dest << ", r" << dest << ", " << ((uint16_t *)&value)[0] << "\n";
+	}
+	else
+	{
+		ss << "li r" << dest << ", " << ((uint16_t *)&value)[0] << "\n";
 	}
 }
 
 void Assembly::moveResister(int src, int dest)
 {
-	ss <<
-		"mr r" << dest << ", r" << src << "\n";
+	ss << "mr r" << dest << ", r" << src << "\n";
 }
 
 int Assembly::push(int src)
 {
-	ss <<
-		"stw r" << src << ", " << stack_size - stack_offset << "(r1)\n";
+	ss << "stw r" << src << ", " << stack_size - stack_offset << "(r1)\n";
 	stack_offset += 4;
 	return stack_size - stack_offset + 4;
 }
 
-
-
 void Assembly::pop(int offset, int dest)
 {
-	ss <<
-		"lwz r" << dest << ", " << offset << "(r1)\n";
+	ss << "lwz r" << dest << ", " << offset << "(r1)\n";
 }
 
 void Assembly::callFunction(int address)
 {
 	writeRegister(address, 12);
-	ss <<
-		"mtctr r12 \n"
-		"bctrl \n";
+	ss << "mtctr r12 \n"
+		  "bctrl \n";
 }
 
 void Assembly::peek(int offset, int dest, int src)
 {
-	ss <<
-		"lwz r" << dest << ", " << offset << "(r" << src << ")\n";
+	ss << "lwz r" << dest << ", " << offset << "(r" << src << ")\n";
 }
 
 void Assembly::peek_i(int address, int offset, int dest)
 {
 	writeRegister(address, 12);
-	ss <<
-		"lwz r" << dest << ", " << offset << "(r12)\n";
+	ss << "lwz r" << dest << ", " << offset << "(r12)\n";
 }
 
 void Assembly::poke(int offset, int dest, int src)
 {
-	ss <<
-		"stw r" << src << ", " << offset << "(r" << dest << ")\n";
+	ss << "stw r" << src << ", " << offset << "(r" << dest << ")\n";
 }
 
 void Assembly::poke_i(int address, int src, int offset)
 {
 	writeRegister(address, 12);
-	ss <<
-		"stw r" << src << ", " << offset << "(r12)\n";
+	ss << "stw r" << src << ", " << offset << "(r12)\n";
 }
 
 void Assembly::add(int value, int src, int dest)
 {
-	ss <<
-		"addi r" << dest << ", r" << src << ", " << value << "\n"; //r(dest) = r3 + r(src)
+	ss << "addi r" << dest << ", r" << src << ", " << value << "\n"; //r(dest) = r3 + r(src)
 }
 void Assembly::mul(int value, int src, int dest)
 {
 	writeRegister(value, 3);
-	ss <<
-		"mullw r" << dest << ", r3" << ", r" << src << "\n"; //r(dest) = r3 + r(src)
+	ss << "mullw r" << dest << ", r3"
+	   << ", r" << src << "\n"; //r(dest) = r3 + r(src)
 }
 
 void Assembly::div(int value, int src, int dest)
 {
 	writeRegister(value, 3);
-	ss <<
-		"divw r" << dest << ", r3" << ", r" << src << "\n"; //r(dest) = r3 + r(src)
+	ss << "divw r" << dest << ", r3"
+	   << ", r" << src << "\n"; //r(dest) = r3 + r(src)
 }
 
 void Assembly::addReg(int valueReg, int src, int dest)
@@ -156,37 +150,31 @@ void Assembly::shtlReg(int valueReg, int src, int dest)
 	ss << "slw r" << dest << ", r" << src << ", r" << valueReg << std::endl;
 }
 
-
 void Assembly::OR(int src_1, int dest, int src_2)
 {
-	ss <<
-		"or r" << dest << ", r" << src_1 << ", r" << src_2 << "\n";
+	ss << "or r" << dest << ", r" << src_1 << ", r" << src_2 << "\n";
 }
 
 void Assembly::XOR(int src_1, int dest, int src_2)
 {
-	ss <<
-		"xor r" << dest << ", r" << src_1 << ", r" << src_2 << "\n";
+	ss << "xor r" << dest << ", r" << src_1 << ", r" << src_2 << "\n";
 }
 
 void Assembly::AND(int src_1, int dest, int src_2)
 {
-	ss <<
-		"and r" << dest << ", r" << src_1 << ", r" << src_2 << "\n";
+	ss << "and r" << dest << ", r" << src_1 << ", r" << src_2 << "\n";
 }
 
 void Assembly::rightShift(int bit, int src, int dest)
 {
 	writeRegister(bit, 3);
-	ss <<
-		"srw r" << dest << ", r" << src << ", r3\n";
+	ss << "srw r" << dest << ", r" << src << ", r3\n";
 }
 
 void Assembly::leftShift(int bit, int src, int dest)
 {
 	writeRegister(bit, 3);
-	ss <<
-		"slw r" << dest << ", r" << src << ", r3\n";
+	ss << "slw r" << dest << ", r" << src << ", r3\n";
 }
 
 void Assembly::startOfLoop(int count, int init)
@@ -194,11 +182,9 @@ void Assembly::startOfLoop(int count, int init)
 	loop_count = count;
 	loop_flag = 0;
 
-	ss <<
-		"li r3, " << init << "\n";
+	ss << "li r3, " << init << "\n";
 	int offset = push(3);
-	ss <<
-		"loop_" << make_loop_ctr << ":\n";
+	ss << "loop_" << make_loop_ctr << ":\n";
 	loop_offset.emplace_back(offset);
 	make_loop_ctr++;
 }
@@ -207,29 +193,41 @@ void Assembly::endOfLoop()
 {
 	int offset = loop_offset[make_loop_ctr - 1];
 	pop(offset, 3);
-	ss <<
-		"addi r3, r3, 1\n"
-		"cmplwi r3, " << loop_count << "\n"
-		"stw r3, " << offset << "(r1)\n"
-		"blt loop_" << make_loop_ctr - 1 << "\n";
+	ss << "addi r3, r3, 1\n"
+		  "cmplwi r3, "
+	   << loop_count << "\n"
+						"stw r3, "
+	   << offset << "(r1)\n"
+					"blt loop_"
+	   << make_loop_ctr - 1 << "\n";
 	make_loop_ctr--;
 }
 
 void Assembly::startOfIf(int src_1, std::string mode, int src_2)
 {
-	ss <<
-		"cmplw r" << src_1 << ", r" << src_2 << "\n";
-	if (mode == "==") {
+	ss << "cmplw r" << src_1 << ", r" << src_2 << "\n";
+	if (mode == "==")
+	{
 		ss << "beq if_" << make_if_ctr << "\n";
-	} else if (mode == "!=") {
+	}
+	else if (mode == "!=")
+	{
 		ss << "beq if_" << make_if_ctr << "\n";
-	} else if (mode == "<") {
+	}
+	else if (mode == "<")
+	{
 		ss << "blt if_" << make_if_ctr << "\n";
-	} else if (mode == "<=") {
+	}
+	else if (mode == "<=")
+	{
 		ss << "ble if_" << make_if_ctr << "\n";
-	} else if (mode == ">") {
+	}
+	else if (mode == ">")
+	{
 		ss << "bgt if_" << make_if_ctr << "\n";
-	} else if (mode == ">=") {
+	}
+	else if (mode == ">=")
+	{
 		ss << "bge if_" << make_if_ctr << "\n";
 	}
 	startOfIf_footer();
@@ -237,23 +235,24 @@ void Assembly::startOfIf(int src_1, std::string mode, int src_2)
 
 void Assembly::startOfIf(condType mode, int condReg)
 {
-	switch (mode) {
+	switch (mode)
+	{
 	case EQU: // ==
 		ss << "beq if_" << make_if_ctr << ", cr" << condReg << "\n";
 		break;
 	case NEQ: // !=
 		ss << "beq if_" << make_if_ctr << ", cr" << condReg << "\n";
 		break;
-	case LT:  // < 
+	case LT: // <
 		ss << "blt if_" << make_if_ctr << ", cr" << condReg << "\n";
 		break;
-	case GT:  // > 
+	case GT: // >
 		ss << "bgt if_" << make_if_ctr << ", cr" << condReg << "\n";
 		break;
-	case GE:  // <=
+	case GE: // <=
 		ss << "bge if_" << make_if_ctr << ", cr" << condReg << "\n";
 		break;
-	case LE:  // >=
+	case LE: // >=
 		ss << "ble if_" << make_if_ctr << ", cr" << condReg << "\n";
 		break;
 	}
@@ -262,16 +261,16 @@ void Assembly::startOfIf(condType mode, int condReg)
 
 void Assembly::startOfIf_footer()
 {
-	ss <<
-		"b " << "endif_" << make_if_ctr << ":\n"
-		"if_" << make_if_ctr << ":\n";
+	ss << "b "
+	   << "endif_" << make_if_ctr << ":\n"
+									 "if_"
+	   << make_if_ctr << ":\n";
 	make_if_ctr++;
 }
 
 void Assembly::endOfIf()
 {
-	ss <<
-		"endif_" << make_if_ctr - 1 << ":\n";
+	ss << "endif_" << make_if_ctr - 1 << ":\n";
 	make_if_ctr--;
 }
 
@@ -286,28 +285,54 @@ void Assembly::compareImm(int reg1, int imm)
 void Assembly::condJump(condType mode, int condReg, uint32_t addr)
 {
 	std::wstring mnemonic;
-	switch (mode) {
-	case EQU: mnemonic = L"beqctrl"; break;
-	case NEQ: mnemonic = L"beqctrl"; break;
-	case LT:  mnemonic = L"bltctrl"; break;
-	case GT:  mnemonic = L"bgtctrl"; break;
-	case GE:  mnemonic = L"bgectrl"; break;
-	case LE:  mnemonic = L"blectrl"; break;
+	switch (mode)
+	{
+	case EQU:
+		mnemonic = L"beqctrl";
+		break;
+	case NEQ:
+		mnemonic = L"beqctrl";
+		break;
+	case LT:
+		mnemonic = L"bltctrl";
+		break;
+	case GT:
+		mnemonic = L"bgtctrl";
+		break;
+	case GE:
+		mnemonic = L"bgectrl";
+		break;
+	case LE:
+		mnemonic = L"blectrl";
+		break;
 	}
 	writeRegister(addr, 12);
 	ss << "mtctr r12\n"
-		<< mnemonic << ", cr" << condReg << "\n";
+	   << mnemonic << ", cr" << condReg << "\n";
 }
 void Assembly::condJump(condType mode, int condReg, std::wstring label)
 {
 	std::wstring mnemonic;
-	switch (mode) {
-	case EQU: mnemonic = L"beq"; break;
-	case NEQ: mnemonic = L"bne"; break;
-	case LT:  mnemonic = L"blt"; break;
-	case GT:  mnemonic = L"bgt"; break;
-	case GE:  mnemonic = L"bge"; break;
-	case LE:  mnemonic = L"ble"; break;
+	switch (mode)
+	{
+	case EQU:
+		mnemonic = L"beq";
+		break;
+	case NEQ:
+		mnemonic = L"bne";
+		break;
+	case LT:
+		mnemonic = L"blt";
+		break;
+	case GT:
+		mnemonic = L"bgt";
+		break;
+	case GE:
+		mnemonic = L"bge";
+		break;
+	case LE:
+		mnemonic = L"ble";
+		break;
 	}
 	ss << mnemonic << " " << label << ", cr" << condReg << "\n";
 }
@@ -348,21 +373,27 @@ std::wstring Assembly::getEnd_While(int id)
 void Assembly::whileEnd(int id)
 {
 	ss << "b W" << id << ":\n"
-		<< getEnd_While(id) << ":\n";
+	   << getEnd_While(id) << ":\n";
 }
 
 void Assembly::setString(std::wstring str, int dest)
 {
-	ss <<
-		"bl " << "str_" << string_offset << "\n";
+	ss << "bl "
+	   << "str_" << string_offset << "\n";
 
-	for (wchar_t ch : str) {
+	for (wchar_t ch : str)
+	{
 		ss << ".word " << (uint16_t)ch << "\n";
 	}
+	ss << ".word 0" << std::endl;
+	if (str.length() & 1 == 0)
+	{
+		ss << ".word 0" << std::endl;
+	}
 
-	ss <<
-		"str_" << string_offset << ":\n"
-		"mflr r" << dest << "\n";
+	ss << "str_" << string_offset << ":\n"
+									 "mflr r"
+	   << dest << "\n";
 
 	string_offset++;
 }
