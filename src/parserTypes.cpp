079ac748f20f17237d781c6bf8b7f29338bb87a6
syoch <hacksyoch@gmail.com>
2021-03-02 17:15:11 +0900

    update: format

--- src/parserTypes.cpp
+++ src/parserTypes.cpp
@@ -2,37 +2,29 @@
 
 parserTypes::parserContext::parserContext()
 {
-    stream=streamType();
-    Asm=new Assembly(stream);
-    iter=iterType();
-    variables=varsType();
-    compiler=nullptr;
+    stream = streamType();
+    Asm = new Assembly(stream);
+    iter = iterType();
+    variables = varsType();
+    compiler = nullptr;
 }
 
-parserTypes::parserContext::parserContext(parserTypes::parserContext& source)
-    : Asm(source.Asm)
-    , iter(source.iter)
-    , variables(source.variables)
-    , puts(source.puts)
-    , compiler(nullptr)
+parserTypes::parserContext::parserContext(parserTypes::parserContext &source)
+    : Asm(source.Asm), iter(source.iter), variables(source.variables), puts(source.puts), compiler(nullptr)
 {
-    stream<<source.stream.rdbuf();
+    stream << source.stream.rdbuf();
 }
 
 parserTypes::ExecFunc::ExecFunc()
-    : args( std::vector<expr>() )
-    , funcId( L"0x00000000" )
-    , type( ADDRESS )
+    : args(std::vector<expr>()), funcId(L"0x00000000"), type(ADDRESS), funcAddr(0)
 {
-
 }
 
 parserTypes::power::power()
-    : type(power::IMM)
-    , imm(0)
+    : type(power::IMM), imm(0)
 {
 }
 
-bool parserTypes::expr::isSingle(){return parts.size()==1;}
-bool parserTypes::expo::isSingle(){return parts.size()==1;}
-bool parserTypes::term::isSingle(){return parts.size()==1;}
+bool parserTypes::expr::isSingle() { return parts.size() == 1; }
+bool parserTypes::expo::isSingle() { return parts.size() == 1; }
+bool parserTypes::term::isSingle() { return parts.size() == 1; }
