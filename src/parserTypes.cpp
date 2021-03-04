4ad0fda6a449931bb7847da63d0cadf33c90aa61
syoch <hacksyoch@gmail.com>
2021-03-04 18:59:49 +0900

    update: implement ctor of ptr

--- src/parserTypes.cpp
+++ src/parserTypes.cpp
@@ -28,3 +28,20 @@ parserTypes::power::power()
 bool parserTypes::expr::isSingle() { return parts.size() == 1; }
 bool parserTypes::expo::isSingle() { return parts.size() == 1; }
 bool parserTypes::term::isSingle() { return parts.size() == 1; }
+
+parserTypes::ptr::ptr()
+    : base(nullptr)
+{
+}
+parserTypes::ptr::ptr(expr &base)
+    : base(std::make_unique<struct expr>(base))
+{
+}
+parserTypes::ptr::ptr(expr *base)
+    : base(std::make_unique<struct expr>(*base))
+{
+}
+parserTypes::expr parserTypes::ptr::getBase()
+{
+    return *base;
+}
