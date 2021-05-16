b2d829d15ff52ed4705c5814647dec4ac7e4d090
syoch <hacksyoch@gmail.com>
2021-05-16 16:31:06 +0900

    fix: argument mismatch

--- src/myassert.cpp
+++ src/myassert.cpp
@@ -5,6 +5,7 @@
 #include <syntaxError.h>
 #include <parserTypes.h>
 
-void assert_fail(const char* fname,const char* funcname,int line,parserTypes::parserContext& ctx,const wchar_t* msg){
-    synErr::syntaxError(ctx,msg,fname,funcname,line);
+void assert_fail(const char *fname, const char *funcname, int line, parserCore *ctx, const wchar_t *msg)
+{
+    synErr::syntaxError(ctx, msg, fname, funcname, line);
 }
