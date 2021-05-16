5f33f1ff954015c3abb1b57695c790c4dec55e8d
syoch <hacksyoch@gmail.com>
2021-05-16 14:59:18 +0900

    update: change parserCore type

--- src/include/parserCore.h
+++ src/include/parserCore.h
@@ -24,45 +24,43 @@ namespace parserTypes
     class expr;
 } // namespace parserTypes
 
-
-namespace parserCore
+class parserCore
 {
-    using Arg=std::pair<std::wstring,std::wstring>;
-    using Range=std::pair<int,int>;
+    using Arg = std::pair<std::wstring, std::wstring>;
+    using Range = std::pair<int, int>;
 
-    int Int  (parserTypes::parserContext&);
-    std::wstring ident(parserTypes::parserContext&);
+    int Int(parserTypes::parserContext &);
+    std::wstring ident(parserTypes::parserContext &);
 
-    Arg arg(parserTypes::parserContext&);
-    struct parserTypes::ptr ptr(parserTypes::parserContext&);
-    
-    struct parserTypes::value editable(parserTypes::parserContext&);
-    struct parserTypes::value constant(parserTypes::parserContext&);
-    struct parserTypes::value value(parserTypes::parserContext&);
-    
-    struct parserTypes::power power (parserTypes::parserContext&);
-    struct parserTypes::expo expo  (parserTypes::parserContext&);
-    struct parserTypes::term term  (parserTypes::parserContext&);
-    struct parserTypes::expr expr  (parserTypes::parserContext&);
+    Arg arg(parserTypes::parserContext &);
+    struct parserTypes::ptr ptr(parserTypes::parserContext &);
 
-    struct parserTypes::cond cond  (parserTypes::parserContext&);
-    struct parserTypes::condAnd condAnd  (parserTypes::parserContext&);
-    struct parserTypes::condChild cond_inner  (parserTypes::parserContext&);
+    struct parserTypes::value editable(parserTypes::parserContext &);
+    struct parserTypes::value constant(parserTypes::parserContext &);
+    struct parserTypes::value value(parserTypes::parserContext &);
 
-    Range range  (parserTypes::parserContext&);
+    struct parserTypes::power power(parserTypes::parserContext &);
+    struct parserTypes::expo expo(parserTypes::parserContext &);
+    struct parserTypes::term term(parserTypes::parserContext &);
+    struct parserTypes::expr expr(parserTypes::parserContext &);
 
-    void program(parserTypes::parserContext&);
-    void stmt(parserTypes::parserContext&);
-    void func(parserTypes::parserContext&);
-    void If(parserTypes::parserContext&);
-    void For(parserTypes::parserContext&);
-    void While(parserTypes::parserContext&);
-    void put(parserTypes::parserContext&);
-    void assign(parserTypes::parserContext&);
-    void mcl(parserTypes::parserContext&);
+    struct parserTypes::cond cond(parserTypes::parserContext &);
+    struct parserTypes::condAnd condAnd(parserTypes::parserContext &);
+    struct parserTypes::condChild cond_inner(parserTypes::parserContext &);
 
-    struct parserTypes::ExecFunc funcCall(parserTypes::parserContext&);
+    Range range(parserTypes::parserContext &);
+
+    void program(parserTypes::parserContext &);
+    void stmt(parserTypes::parserContext &);
+    void func(parserTypes::parserContext &);
+    void If(parserTypes::parserContext &);
+    void For(parserTypes::parserContext &);
+    void While(parserTypes::parserContext &);
+    void put(parserTypes::parserContext &);
+    void assign(parserTypes::parserContext &);
+    void mcl(parserTypes::parserContext &);
+
+    struct parserTypes::ExecFunc funcCall(parserTypes::parserContext &);
 } // namespace parserWrap
-
 
 #endif
