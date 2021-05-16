4773ca163d14eee2f40e3789c9f18d020adedc67
syoch <hacksyoch@gmail.com>
2021-05-16 16:07:31 +0900

    refactor: format

--- src/syntaxError.cpp
+++ src/syntaxError.cpp
@@ -3,40 +3,47 @@
 
 using namespace synErr;
 
-void synErr::ReportError(parserTypes::parserContext& ctx,errorType type,std::wstring exMsg,const char* fname,const char* funcname,int line){
+void synErr::ReportError(parserTypes::parserContext &ctx, errorType type, std::wstring exMsg, const char *fname, const char *funcname, int line)
+{
     std::wstring strType;
-    if(type==SYNTAX){
-        strType=L"syntax";
-    }else if(type==TOKENIZE){
-        strType=L"tokenize";
-    }else if(type==PROCESS){
-        strType=L"process";
+    if (type == SYNTAX)
+    {
+        strType = L"syntax";
+    }
+    else if (type == TOKENIZE)
+    {
+        strType = L"tokenize";
+    }
+    else if (type == PROCESS)
+    {
+        strType = L"process";
     }
 
-    std::wstring prev=ctx.iter.peekSafe(-1);
-    std::wstring curr=ctx.iter.peekSafe( 0);
-    std::wstring next=ctx.iter.peekSafe( 1);
-    std::wstring pos=prev+curr+next;
-    
-    std::wstring offset=std::wstring(prev.length(),L' ');
-    std::wstring uline =std::wstring(curr.length(),L'^');
+    std::wstring prev = ctx.iter.peekSafe(-1);
+    std::wstring curr = ctx.iter.peekSafe(0);
+    std::wstring next = ctx.iter.peekSafe(1);
+    std::wstring pos = prev + curr + next;
+
+    std::wstring offset = std::wstring(prev.length(), L' ');
+    std::wstring uline = std::wstring(curr.length(), L'^');
     std::wcout
-        << "-------Error--------"               <<std::endl
-        << "| type:"<<strType                   <<std::endl;
-    if(exMsg!=L"")std::wcout
-        << "| msg :"<<exMsg                     <<std::endl;
+        << "-------Error--------" << std::endl
+        << "| type:" << strType << std::endl;
+    if (exMsg != L"")
+        std::wcout
+            << "| msg :" << exMsg << std::endl;
     std::wcout
-        << "| Where?"                           <<std::endl
-        << "|  func: "<<funcname                <<std::endl
-        << "|  copyable: "<<fname<<":"<<line    <<std::endl
-        << "| Pos Informations"                 <<std::endl
-        << "| "<<pos                            <<std::endl
-        << "| "<<offset<<uline                  <<std::endl;
+        << "| Where?" << std::endl
+        << "|  func: " << funcname << std::endl
+        << "|  copyable: " << fname << ":" << line << std::endl
+        << "| Pos Informations" << std::endl
+        << "| " << pos << std::endl
+        << "| " << offset << uline << std::endl;
     std::wcout
-        << "--------------------"               <<std::endl;
-    
+        << "--------------------" << std::endl;
+
     throw exMsg;
 }
-void synErr::syntaxError  (parserTypes::parserContext& ctx,std::wstring ex,const char* fname,const char* funcname,int line) {ReportError(ctx,SYNTAX,ex,fname,funcname,line);}
-void synErr::tokenizeError(parserTypes::parserContext& ctx,std::wstring ex,const char* fname,const char* funcname,int line) {ReportError(ctx,TOKENIZE,ex,fname,funcname,line);}
-void synErr::processError (parserTypes::parserContext& ctx,std::wstring ex,const char* fname,const char* funcname,int line) {ReportError(ctx,PROCESS,ex,fname,funcname,line);}
+void synErr::syntaxError(parserTypes::parserContext &ctx, std::wstring ex, const char *fname, const char *funcname, int line) { ReportError(ctx, SYNTAX, ex, fname, funcname, line); }
+void synErr::tokenizeError(parserTypes::parserContext &ctx, std::wstring ex, const char *fname, const char *funcname, int line) { ReportError(ctx, TOKENIZE, ex, fname, funcname, line); }
+void synErr::processError(parserTypes::parserContext &ctx, std::wstring ex, const char *fname, const char *funcname, int line) { ReportError(ctx, PROCESS, ex, fname, funcname, line); }
