c339dca1cd46136d72d4e7971f662c670c25f0af
syoch <hacksyoch@gmail.com>
2021-05-16 16:57:29 +0900

    update: remove parserContext ctor

--- src/parserTypes.cpp
+++ src/parserTypes.cpp
@@ -1,20 +1,5 @@
 #include <parserTypes.h>
 
-parserTypes::parserContext::parserContext()
-{
-    stream = streamType();
-    Asm = new Assembly(stream);
-    iter = iterType();
-    variables = varsType();
-    compiler = nullptr;
-}
-
-parserTypes::parserContext::parserContext(parserTypes::parserContext &source)
-    : Asm(source.Asm), iter(source.iter), variables(source.variables), puts(source.puts), compiler(nullptr)
-{
-    stream << source.stream.rdbuf();
-}
-
 parserTypes::ExecFunc::ExecFunc()
     : args(std::vector<expr>()), funcId(L"0x00000000"), type(ADDRESS), funcAddr(0)
 {
