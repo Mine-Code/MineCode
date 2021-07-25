2973c6c99afc7e407a6378cfcc1f3d4f4662cb69
syoch <hacksyoch@gmail.com>
2021-07-26 08:36:59 +0900

    fix: add value ctor

--- src/src/value/value.cpp
+++ src/src/value/value.cpp
@@ -1,3 +1,4 @@
 #include <value/value.hpp>
+parserTypes::value::BaseValue::BaseValue() {}
 parserTypes::value::BaseValue::~BaseValue() {}
 void parserTypes::value::BaseValue::eval(parserCore& ctx, int dest) {}
