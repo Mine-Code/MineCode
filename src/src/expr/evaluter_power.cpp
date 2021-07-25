f5da15a8f65e02c38a0a66d7552bad5fe8928b38
syoch <hacksyoch@gmail.com>
2021-07-26 01:12:30 +0900

    update: fix power evaluter

--- src/src/expr/evaluter_power.cpp
+++ src/src/expr/evaluter_power.cpp
@@ -7,34 +7,8 @@
 
 using namespace parserTypes;
 
-void eval::Power(parserCore *that, primary obj, int dest) {
+void eval::Power(parserCore *that, primary::BasePrimary &obj, int dest) {
   int offs = that->Asm->stack_offset;
-  switch (obj.type) {
-    case primary::EXPR:
-      Expr(that, obj.expr, dest);
-      break;
-    case primary::FLT:
-      synErr::processError(that, L"Float isn't supported...", __FILE__,
-                           __func__, __LINE__);
-      break;
-    case primary::FUNCCALL:
-      stmtProcessor::executeFunction(that, *obj.func);
-      that->Asm->moveResister(3, dest);
-      break;
-    case primary::IMM:
-      that->Asm->writeRegister(obj.imm, dest);
-      break;
-    case primary::PTR:
-      Ptr(that, obj.Pointer, dest);
-      break;
-    case primary::VAR:
-      Var(that, obj.var, dest);
-      break;
-    default:
-      synErr::processError(
-          that, L"unknown type error [" + std::to_wstring(obj.type) + L"]",
-          __FILE__, __func__, __LINE__);
-      break;
-  }
+  obj.eval(*that);
   that->Asm->stack_offset = offs;
 }
