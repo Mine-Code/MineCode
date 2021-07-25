0edae073b7b0204b7902e7c9d17e4dc41c23368a
syoch <hacksyoch@gmail.com>
2021-07-26 01:18:52 +0900

    update: change vector type

--- src/include/expr/expo.hpp
+++ src/include/expr/expo.hpp
@@ -4,7 +4,7 @@
 #include <vector>
 namespace parserTypes {
 struct expo {
-  std::vector<struct primary::BasePrimary> parts;
+  std::vector<primary::BasePrimary*> parts;
   bool isSingle();
 };
 }  // namespace parserTypes
