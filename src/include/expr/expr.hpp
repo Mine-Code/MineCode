921e7405e7381572c741e9bb034cd6049a04bfdf
syoch <hacksyoch@gmail.com>
2021-07-25 19:57:53 +0900

    refactor: remove old include cguard

--- src/include/expr/expr.hpp
+++ src/include/expr/expr.hpp
@@ -1,10 +1,4 @@
-
 #pragma once
-#ifndef PARSER_EVAL_H
-#define PARSER_EVAL_H
-
-#include <memory>
-#include <vector>
 
 #include "expo.hpp"
 #include "expo_wrap.hpp"
@@ -13,4 +7,3 @@
 #include "ptr.hpp"
 #include "term.hpp"
 #include "value.hpp"
-#endif
