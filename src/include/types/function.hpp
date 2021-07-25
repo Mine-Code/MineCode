64826fcfbbebce777f0874839f093ee183584f47
syoch <hacksyoch@gmail.com>
2021-07-25 19:16:28 +0900

    update: move expr.hpp

--- src/include/types/function.hpp
+++ src/include/types/function.hpp
@@ -2,27 +2,19 @@
 #ifndef PARSER_FUNCTION_H
 #define PARSER_FUNCTION_H
 #include <inttypes.h>
-#include <vector>
-#include "expr.hpp"
 
-namespace parserTypes
-{
-  enum funcArgType
-  {
-    INT,
-    CSTR,
-    WSTR,
-    PTR
-  };
-  struct funcArg
-  {
-    funcArgType type;
-    struct expr defaultValue;
-  };
-  struct function
-  {
-    uint32_t addr;
-    std::vector<struct funcArg> args;
-  };
-}
+#include <expr/expr.hpp>
+#include <vector>
+
+namespace parserTypes {
+enum funcArgType { INT, CSTR, WSTR, PTR };
+struct funcArg {
+  funcArgType type;
+  struct expr defaultValue;
+};
+struct function {
+  uint32_t addr;
+  std::vector<struct funcArg> args;
+};
+}  // namespace parserTypes
 #endif
