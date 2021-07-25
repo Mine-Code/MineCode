cebc337c541753bfa32fcbe8fa3ced19af2230f5
syoch <hacksyoch@gmail.com>
2021-07-26 07:02:28 +0900

    update: impl Immutable

--- src/src/primary/immutable.cpp
+++ src/src/primary/immutable.cpp
@@ -1,3 +1,8 @@
+#include <asm.h>
+#include <parserCore.h>
+
 #include <primary/immutable.hpp>
 parserTypes::primary::Immutable::Immutable(int val) : value(val) {}
-void parserTypes::primary::Immutable::eval(parserCore& ctx, int dest) {}
+void parserTypes::primary::Immutable::eval(parserCore& ctx, int dest) {
+  ctx.Asm->writeRegister(this->value, dest);
+}
