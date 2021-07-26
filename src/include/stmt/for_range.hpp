38a5919bc30ad079977683e749ac57d5582d459e
syoch <hacksyoch@gmail.com>
2021-07-26 18:41:39 +0900

    update: add ForRange

---
+++ src/include/stmt/for_range.hpp
@@ -0,0 +1,14 @@
+#pragma once
+
+#include "stmt.hpp"
+
+namespace parserTypes {
+namespace stmt {
+class ForRange {
+  int start;
+  int end;
+  virtual void exec(parserCore& ctx);
+};
+}  // namespace stmt
+
+}  // namespace parserTypes
