908cbc21d15738a5a664f7886dd2f0407e63e28b
syoch <hacksyoch@gmail.com>
2021-07-27 21:00:02 +0900

    update: add alias header

---
+++ src/include/stmt/all.hpp
@@ -0,0 +1,8 @@
+#include <stmt/assign.hpp>
+#include <stmt/exec_func.hpp>
+#include <stmt/for_iterator.hpp>
+#include <stmt/for_range.hpp>
+#include <stmt/funcdef.hpp>
+#include <stmt/if.hpp>
+#include <stmt/put.hpp>
+#include <stmt/while.hpp>
