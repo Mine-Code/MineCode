b223e53030901fd8dac5b2ee615fa8705ac1529e
syoch <hacksyoch@gmail.com>
2021-07-28 18:32:46 +0900

    update: add consume in typediterator

--- src/include/typedIterator.hxx
+++ src/include/typedIterator.hxx
@@ -2,61 +2,65 @@
 #ifndef CHITER_H
 #define CHITER_H
 
-#include <vector>
 #include <iostream>
+#include <vector>
 
 template <typename T>
-class iterator
-{
-private:
-    std::vector<T> array;
-public:
-    size_t index;
-    iterator()
-        :array(), index(0)
-    {
-        array=std::vector<T>();
-    }
-    iterator(std::vector<T> src)
-        :array(src), index(0)
-    {}
-    T next(){
-        if(array.size() <= index){
-            std::cerr << "Out of size" << std::endl;;
-            throw L"outofsize";
-        }
-        return array[index++];
-    }
-    T prev(){
-        if(index < 0){
-            std::cerr << "Out of size" << std::endl;;
-            throw L"outofsize";
-        }
-        return array[index--];
-    }
-    T peekSafe(size_t offset=0){
-        if(index+offset < 0 || array.size() <= index+offset){
-            return T();
-        }
-        return array[index+offset];
-    }
-    T peek(size_t offset=0){
-        if(index < 0 || array.size() <= index){
-            std::cerr << "Out of size" << std::endl;;
-            throw L"outofsize";
-        }
-        return array[index+offset];
-    }
-    bool hasData(){
-        if(array.size() <= index){
-            return false;
-        }
-        return true;
-    }
+class iterator {
+ private:
+  std::vector<T> array;
 
-    void clear(){
-        this->array.clear();
+ public:
+  size_t index;
+  iterator() : array(), index(0) { array = std::vector<T>(); }
+  iterator(std::vector<T> src) : array(src), index(0) {}
+  T next() {
+    if (array.size() <= index) {
+      std::cerr << "Out of size" << std::endl;
+      ;
+      throw L"outofsize";
     }
+    return array[index++];
+  }
+  T prev() {
+    if (index < 0) {
+      std::cerr << "Out of size" << std::endl;
+      ;
+      throw L"outofsize";
+    }
+    return array[index--];
+  }
+  T peekSafe(size_t offset = 0) {
+    if (index + offset < 0 || array.size() <= index + offset) {
+      return T();
+    }
+    return array[index + offset];
+  }
+  T peek(size_t offset = 0) {
+    if (index < 0 || array.size() <= index) {
+      std::cerr << "Out of size" << std::endl;
+      ;
+      throw L"outofsize";
+    }
+    return array[index + offset];
+  }
+  bool consume(T token) {
+    auto val = peek();
+    if (val == token) {
+      index++;
+      return true;
+    } else {
+      return false;
+    }
+  }
+  bool hasData() {
+    if (array.size() <= index) {
+      return false;
+    }
+    return true;
+  }
+
+  void clear() { this->array.clear(); }
 };
 
 #endif
