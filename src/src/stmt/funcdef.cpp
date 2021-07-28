104c1838749e0056d7880cb235ee92dd2c6493ca
syoch <hacksyoch@gmail.com>
2021-07-29 07:05:55 +0900

    update: fix funcdef

--- src/src/stmt/funcdef.cpp
+++ src/src/stmt/funcdef.cpp
@@ -1,5 +1,6 @@
 #include <asm.h>
 #include <parserCore.h>
+#include <util.h>
 
 #include <stmt/funcdef.hpp>
 parserTypes::stmt::FuncDef::~FuncDef() {}
@@ -7,12 +8,15 @@ void parserTypes::stmt::FuncDef::exec(parserCore& ctx) {
   auto previous_funcname = ctx.funcname;
 
   ctx.funcname = this->name;
+  ctx.Asm->Jump(this->name + L"return");
+  ctx.stream << "# Inner Function ";
+  ctx.stream << this->name;
+  ctx.stream << "\n";
 
-  ctx.stream << "# Inner Function\n";
-  while (ctx.iter.hasData()) {
-    if (ctx.iter.peek() == L"}") break;
-    ctx.stmt();
+  for (auto&& stmt : this->stmts) {
+    stmt->exec(ctx);
   }
+
   ctx.stream << "# Outer Function\n";
   ctx.Asm->makeLabel(this->name + L"_return");
 }
