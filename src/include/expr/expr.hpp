fbe07604e3326b78c87a030d89ec6467591ba87b
syoch <hacksyoch@gmail.com>
2021-07-29 10:35:47 +0900

    update: remove expo_wrap

--- src/include/expr/expr.hpp
+++ src/include/expr/expr.hpp
@@ -3,7 +3,6 @@
 #include <primary/primary.hpp>
 
 #include "expo.hpp"
-#include "expo_wrap.hpp"
 #include "expr.hpp"
 #include "ptr.hpp"
 #include "term.hpp"
--- src/include/expr/term.hpp
+++ src/include/expr/term.hpp
@@ -2,8 +2,15 @@
 
 #include <vector>
 
-#include "expo_wrap.hpp"
+#include "expo.hpp"
+
 namespace parserTypes {
+struct expo_wrap {
+  enum Type { MUL, DIV, MOD };
+
+  Type type;
+  struct expo value;
+};
 struct term {
   std::vector<struct expo_wrap> parts;
   bool isSingle();
