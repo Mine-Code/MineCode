b7465ad4c1981518f075caef2c4637ddbf610e16
syoch <hacksyoch@gmail.com>
2021-07-29 10:40:38 +0900

    update: fix BaseExpr declare

--- src/include/expr/base.hpp
+++ src/include/expr/base.hpp
@@ -4,7 +4,8 @@
 class parserCore;
 namespace parserTypes {
 struct BaseExpr {
+  virtual ~BaseExpr();
   virtual void eval(parserCore& ctx) = 0;
-  [[nodiscard("Not changed.")]] virtual BaseExpr& optimize() = 0;
+  [[nodiscard]] virtual BaseExpr& optimize() = 0;
 };
 }  // namespace parserTypes
