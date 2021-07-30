19587bc710f7b4cb103f038be8fe9597932c6763
syoch <hacksyoch@gmail.com>
2021-07-30 13:01:15 +0900

    update: remove removed type

--- src/src/parserTypes.cpp
+++ src/src/parserTypes.cpp
@@ -5,13 +5,3 @@ parserTypes::ExecFunc::ExecFunc()
       funcId(L"0x00000000"),
       funcAddr(0),
       type(ADDRESS) {}
-
-bool parserTypes::Expr::isSingle() { return parts.size() == 1; }
-bool parserTypes::expo::isSingle() { return parts.size() == 1; }
-bool parserTypes::term::isSingle() { return parts.size() == 1; }
-
-parserTypes::ptr::ptr() : base(nullptr) {}
-parserTypes::ptr::ptr(Expr &base) : base(std::make_unique<struct Expr>(base)) {}
-parserTypes::ptr::ptr(Expr *base)
-    : base(std::make_unique<struct Expr>(*base)) {}
-parserTypes::Expr parserTypes::ptr::getBase() { return *base; }
