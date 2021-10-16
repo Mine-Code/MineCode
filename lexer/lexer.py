7a45fc54549ffa883be062d8c5c748ab852c2a8f
syoch <hacksyoch@gmail.com>
2021-10-16 00:55:26 -0400

    update: impl lexer

--- lexer/lexer.py
+++ lexer/lexer.py
@@ -1,4 +1,20 @@
-class Lexer:
-    def __init__(self):
-        self.src: str = ""
-        self.pos: int = 0
+from typing import Iterable
+from .line import Line
+from .line_splitter import LineSplitter
+from .tokenizer import Tokenizer
+
+tokenizer = Tokenizer()
+
+
+def _lex(lines: Iterable[Line]) -> Iterable[Line]:
+    for line in lines:
+        tokenizer.set_input(line.line)
+        line.tokens = [*tokenizer.tokenize()]
+        line.children = [*_lex(line.children)]
+
+        yield line
+
+
+def lex(src: str) -> Iterable[Line]:
+    lines = LineSplitter(src).split()
+    return _lex(lines)
