0b219c2c5a5031c87089dc9a47f904e395e5b21f
syoch <hacksyoch@gmail.com>
2021-10-16 00:55:57 -0400

    update: Added lexer test

--- __main__.py
+++ __main__.py
@@ -1,13 +1,7 @@
-from .lexer.tokenizer import Tokenizer
+from .lexer.lexer import lex
 
 with open("MineCode/tests/alltest", "r") as f:
     src = f.read()
-#
-# splitter = LineSplitter(src)
-# for line in splitter.split():
-#     print(line)
 
-tokenizer = Tokenizer()
-tokenizer.set_input(src)
-for token in tokenizer.tokenize():
-    print(token)
+for line in lex(src):
+    print(line)
