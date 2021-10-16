ab539a32f58f1fb559077ff7be0e1b8f38c6f518
syoch <hacksyoch@gmail.com>
2021-10-15 23:20:08 -0400

    tests: wip

--- __main__.py
+++ __main__.py
@@ -1,13 +1,13 @@
 from .lexer.tokenizer import Tokenizer
 
-# with open("MineCode/tests/alltest", "r") as f:
-#     src = f.read()
+with open("MineCode/tests/alltest", "r") as f:
+    src = f.read()
 #
 # splitter = LineSplitter(src)
 # for line in splitter.split():
 #     print(line)
 
 tokenizer = Tokenizer()
-tokenizer.set_input('"hello" 123 0x123 0o123 0b110')
+tokenizer.set_input(src)
 for token in tokenizer.tokenize():
     print(token)
