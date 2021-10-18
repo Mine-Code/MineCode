bc8d58caf3b4b1132fa7213d9c159811129ad403
syoch <hacksyoch@gmail.com>
2021-10-18 09:50:47 -0400

    update: impl some text visualizer

--- lexer/layer.py
+++ lexer/layer.py
@@ -11,8 +11,20 @@ class Layer(Element):
         super().__init__()
         self.tokens: List[Element] = elements
 
+    def __str__(self) -> str:
+        return str(self.tokens)
+
+    def __repr__(self) -> str:
+        return str(self)
+
 
 class Token(Element):
     def __init__(self, token: RawToken):
         super().__init__()
         self.token = token
+
+    def __str__(self) -> str:
+        return str(self.token)
+
+    def __repr__(self) -> str:
+        return str(self)
