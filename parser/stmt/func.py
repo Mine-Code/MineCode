1ad191de1a3e0c6631cb60a70d59d4df71d26b14
syoch <hacksyoch@gmail.com>
2021-10-20 07:29:08 -0400

    update: change Func's constructor declare

--- parser/stmt/func.py
+++ parser/stmt/func.py
@@ -4,10 +4,11 @@ from ...lexer.token import IdentifierToken
 
 
 class Func(Stmt):
-    def __init__(self, func_name: str, args: Iterable[IdentifierToken]):
+    def __init__(self, func_name: str, args: Iterable[IdentifierToken], body: Iterable[Stmt]):
         super().__init__()
         self.name = func_name
         self.args = args
+        self.body = body
 
     def __str__(self):
         return f"Func<{self.name}>({self.args})"
