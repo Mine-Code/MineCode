2046d122efa57b74db578403d351ad8cc527e3d9
syoch <hacksyoch@gmail.com>
2021-10-25 06:56:08 -0400

    update: Added [left|right]_[inc|dec].py

---
+++ parser/expr/unary/left_dec.py
@@ -0,0 +1,12 @@
+from ..expr import ExprBase
+
+
+class LeftDec(ExprBase):
+    def __init__(self, expr):
+        self.expr = expr
+
+    def __str__(self):
+        return '--' + str(self.expr)
+
+    def __repr__(self):
+        return 'LeftDec(' + repr(self.expr) + ')'
---
+++ parser/expr/unary/right_inc.py
@@ -0,0 +1,12 @@
+from ..expr import ExprBase
+
+
+class RightInc(ExprBase):
+    def __init__(self, expr):
+        self.expr = expr
+
+    def __str__(self):
+        return str(self.expr) + '++'
+
+    def __repr__(self):
+        return 'RightInc(' + repr(self.expr) + ')'
