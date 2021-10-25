553c7bb042eb57719021961f90ccd8696293cf7c
syoch <hacksyoch@gmail.com>
2021-10-25 06:56:22 -0400

    update: removed some function in exprbase

--- parser/expr/expr.py
+++ parser/expr/expr.py
@@ -2,11 +2,8 @@ class ExprBase:
     def __init__(self):
         pass
 
-    def eval(self):
-        raise NotImplementedError
-
     def __str__(self):
         raise NotImplementedError
 
     def __repr__(self):
-        return self.__str__()
+        raise NotImplementedError
