2e6aab77d7d6a8adbb5dc9329662dd115a85f68e
syoch <hacksyoch@gmail.com>
2021-10-26 03:31:37 -0400

    update: Added pointer vaue type

--- parser/expr/value/pointer.py
+++ parser/expr/value/pointer.py
@@ -1,7 +1,13 @@
-from ..expr
+from ..exprbase import ExprBase
+from .value import Value
 
 
 class Pointer(Value):
-    def __init__(self, expr:):
-        self.base = base
-        self.offset = offset
+    def __init__(self, expr: ExprBase):
+        self.expr = expr
+
+    def __str__(self):
+        return f'[{self.expr}]'
+
+    def __repr__(self):
+        return f'Pointer({self.expr})'
