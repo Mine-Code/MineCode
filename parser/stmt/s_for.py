0b56b3ed3a0bd10ead97ec3ba192b13f55c106ed
syoch <hacksyoch@gmail.com>
2021-10-26 04:49:06 -0400

    update: added argument of for which is body

--- parser/stmt/s_for.py
+++ parser/stmt/s_for.py
@@ -1,4 +1,4 @@
-from typing import Union
+from typing import List, Union
 
 from ..expr.value import Value, Range
 from .stmt import Stmt
@@ -7,10 +7,14 @@ Iterable = Union[Value, Range]
 
 
 class For(Stmt):
-    def __init__(self, target: str, iter: Iterable):
+    def __init__(self, target: str, iter: Iterable, body: List[Stmt]):
         super().__init__()
         self.target = target
         self.iter = iter
+        self.body = body
 
     def __str__(self):
-        return f"For<{self.target} in {self.iter}>"
+        return f"For<{self.target} in {self.iter}> {{\n{self.body}\n}}"
+
+    def __repr__(self):
+        return f"For<{self.target} in {self.iter}, {self.body}>"
