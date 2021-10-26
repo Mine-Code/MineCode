01775f5e1ea773cd8122df9b36db75005aaad62f
syoch <hacksyoch@gmail.com>
2021-10-26 04:50:04 -0400

    update: impl stmt_expr

---
+++ parser/stmt/expr.py
@@ -0,0 +1,12 @@
+from ..expr.exprbase import ExprBase
+
+
+class Stmt:
+    def __init__(self, expr: ExprBase):
+        self.expr = expr
+
+    def __str__(self):
+        return str(self.expr)
+
+    def __repr__(self):
+        return str(self)
