9682727acc65eec98139552b4e9279e644be3862
syoch <hacksyoch@gmail.com>
2021-10-26 04:37:38 -0400

    update: impl range

---
+++ parser/expr/value/__init__.py
@@ -0,0 +1,8 @@
+from .funccall import FuncCall, NamedFuncCall, RawFuncCall
+from .identifier import Identifier
+from .number import Number
+from .pointer import Pointer
+from .put import Put
+from .range import Range, _Range
+from .str import String
+from .value import Value
--- parser/expr/value/range.py
+++ parser/expr/value/range.py
@@ -1,11 +1,25 @@
 from typing import Union
 from .value import Value
+from .. import value
 
-_Range = Union[]
+_Range = Union[
+    value.Pointer,
+    value.Identifier,
+    value.Identifier,
+    value.Number,
+    value.FuncCall,
+    value.Put
+]
 
 
 class Range(Value):
 
-    def __init__(self, start, end):
+    def __init__(self, start: _Range, end: _Range):
         self.start = start
         self.end = end
+
+    def __str__(self):
+        return f'({self.start})...({self.end})'
+
+    def __repr__(self):
+        return f'Range({self.start}, {self.end})'
