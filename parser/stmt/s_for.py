345e008c694621627d551eb9f8686ba71fd9a5f2
syoch <hacksyoch@gmail.com>
2021-10-26 04:42:56 -0400

    update: impl for

--- parser/stmt/s_for.py
+++ parser/stmt/s_for.py
@@ -1,9 +1,16 @@
+from typing import Union
+
+from ..expr.value import Value, Range
 from .stmt import Stmt
+
+Iterable = Union[Value, Range]
 
 
 class For(Stmt):
-    def __init__(self, target: str, iter):
+    def __init__(self, target: str, iter: Iterable):
         super().__init__()
+        self.target = target
+        self.iter = iter
 
     def __str__(self):
-        return f"For<{self.name}>({self.args})"
+        return f"For<{self.target} in {self.iter}>"
