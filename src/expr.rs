6934ff7b977aa0f29987b77b6a59c7c70865f992
syoch <hacksyoch@gmail.com>
2022-08-06 10:41:20 +0900

    Update: Implement expression parser

--- src/expr.rs
+++ src/expr.rs
@@ -6,8 +6,8 @@ use nom::branch::{alt, permutation};
 use nom::bytes::complete::{tag, take_until};
 use nom::character::complete::{digit1, multispace0, one_of};
 use nom::combinator::opt;
-use nom::multi::{many1, separated_list0};
-use nom::sequence::delimited;
+use nom::multi::{many0, many1, separated_list0};
+use nom::sequence::{delimited, preceded};
 use nom::{IResult, Parser};
 
 use crate::basic::{self, ident, symbol};
@@ -52,6 +52,8 @@ pub enum BinaryOp {
     GreaterThan,
     #[strum(serialize = ">=")]
     GreaterThanOrEqual,
+    #[strum(serialize = "=")]
+    Assignment,
 }
 
 #[derive(Debug)]
@@ -67,72 +69,50 @@ pub enum Primary {
 
     ApplyOperator(BinaryOp, Box<Primary>, Box<Primary>),
 
-    Assign(Box<Primary>, Box<Primary>),
+    LogicalNot(Box<Primary>),
+    BitwiseNot(Box<Primary>),
+    Negative(Box<Primary>),
+
     Subscript(Box<Primary>, Box<Primary>),
 }
 
-fn apply_operator(operators: &[BinaryOp]) -> impl Fn(&str) -> IResult<&str, Primary> {
-    let operators = operators
-        .iter()
-        .map(|x| x.to_string())
-        .collect::<Vec<_>>()
-        .join("");
-    move |input| {
-        let (input, root) = Primary::primary(input)?;
-
-        let (input, parts) = many1(permutation((
-            delimited(multispace0, one_of(operators.as_str()), multispace0),
-            Primary::read,
-        )))(input)?;
-
-        let mut expr = root;
-
-        for (op, right) in parts {
-            expr = Primary::ApplyOperator(
-                BinaryOp::from_str(op.to_string().as_str()).unwrap(),
-                Box::new(expr),
-                Box::new(right),
-            );
-        }
-        Ok((input, expr))
-    }
-}
-
 impl Primary {
-    pub fn num_hex(input: &str) -> IResult<&str, Self> {
+    fn _num_hex(input: &str) -> IResult<&str, Self> {
         let (input, _) = tag("0x")(input)?;
         let (input, num) = many1(one_of("0123456789abcdefABCDEFxX"))(input)?;
 
         let num = num.iter().collect::<String>();
         Ok((input, Primary::Num(i32::from_str_radix(&num, 16).unwrap())))
     }
-    pub fn num_oct(input: &str) -> IResult<&str, Self> {
+    fn _num_oct(input: &str) -> IResult<&str, Self> {
         let (input, _) = tag("0o")(input)?;
         let (input, num) = many1(one_of("01234567"))(input)?;
 
         let num = num.iter().collect::<String>();
         Ok((input, Primary::Num(i32::from_str_radix(&num, 8).unwrap())))
     }
-    pub fn num_dec(input: &str) -> IResult<&str, Self> {
+    fn _num_dec(input: &str) -> IResult<&str, Self> {
         let (input, num) = many1(digit1)(input)?;
 
         let num = num.join("");
         Ok((input, Primary::Num(i32::from_str(&num).unwrap())))
     }
-
-    pub fn num_bin(input: &str) -> IResult<&str, Self> {
+    fn _num_bin(input: &str) -> IResult<&str, Self> {
         let (input, _) = tag("0b")(input)?;
         let (input, num) = many1(one_of("01"))(input)?;
 
         let num = num.iter().collect::<String>();
         Ok((input, Primary::Num(i32::from_str_radix(&num, 2).unwrap())))
     }
-
-    pub fn num(input: &str) -> IResult<&str, Self> {
-        alt((Self::num_hex, Self::num_oct, Self::num_dec, Self::num_bin))(input)
+    fn _num(input: &str) -> IResult<&str, Self> {
+        alt((
+            Self::_num_hex,
+            Self::_num_oct,
+            Self::_num_dec,
+            Self::_num_bin,
+        ))(input)
     }
-
-    pub fn ident(input: &str) -> IResult<&str, Self> {
+    fn _ident(input: &str) -> IResult<&str, Self> {
         let (mut input, mut num) = ident(input)?;
 
         loop {
@@ -156,8 +136,7 @@ impl Primary {
 
         Ok((input, Self::Ident(num)))
     }
-
-    pub fn call_function(input: &str) -> IResult<&str, Self> {
+    fn _call_function(input: &str) -> IResult<&str, Self> {
         let (input, f) = permutation((
             ident,
             delimited(
@@ -169,45 +148,75 @@ impl Primary {
 
         Ok((input, Self::FuncCall(f.0, f.1)))
     }
-
-    pub fn string(input: &str) -> IResult<&str, Self> {
+    fn _string(input: &str) -> IResult<&str, Self> {
         let (input, s) =
             delimited(basic::symbol('"'), take_until("\""), basic::symbol('"'))(input)?;
 
         Ok((input, Self::String(s.to_string())))
     }
+}
 
-    fn primary(input: &str) -> IResult<&str, Self> {
-        let (input, comptime_flag) = opt(basic::symbol('@'))(input)?;
+impl Primary {
+    fn _binary_op<'a>(
+        i: &'a str,
+        joiner: impl FnMut(&'a str) -> IResult<&'a str, &'a str>,
+        parser: impl FnMut(&'a str) -> IResult<&'a str, Primary> + Copy,
+    ) -> IResult<&'a str, Primary> {
+        permutation((
+            parser.clone(),
+            many0(permutation((
+                delimited(multispace0, joiner, multispace0),
+                parser,
+            ))),
+        ))
+        .map(|(mut r, parts)| {
+            for (op, x) in parts {
+                r = Self::ApplyOperator(BinaryOp::from_str(op).unwrap(), Box::new(r), Box::new(x));
+            }
+            r
+        })
+        .parse(i)
+    }
 
-        let (input, mut expr) = alt((
+    fn _primary(input: &str) -> IResult<&str, Self> {
+        let parser = alt((
+            preceded(basic::symbol('-'), Self::read.map(Box::new)).map(|e| Self::Negative(e)),
+            preceded(basic::symbol('~'), Self::read.map(Box::new)).map(|e| Self::BitwiseNot(e)),
+            preceded(basic::symbol('!'), Self::read.map(Box::new)).map(|e| Self::LogicalNot(e)),
+            // CompileTime
+            preceded(basic::symbol('@'), Self::read.map(Box::new)).map(|e| Self::CompileTime(e)),
             // []
             delimited(
                 basic::symbol('['),
                 Self::read.map(Box::new),
                 basic::symbol(']'),
             )
-            .map(|e| Primary::Pointer(e)),
+            .map(|e| Self::Pointer(e)),
             //
-            Self::num,
-            Self::string,
-            Self::call_function,
-            Self::ident,
-        ))(input)?;
+            Self::_num,
+            Self::_string,
+            Self::_call_function,
+            Self::_ident,
+        ));
+        let parser = permutation((parser, opt(delimited(symbol('{'), Self::read, symbol('}')))))
+            .map(|(array, index)| {
+                if index.is_none() {
+                    array
+                } else {
+                    Self::Subscript(Box::new(array), Box::new(index.unwrap()))
+                }
+            });
 
-        if comptime_flag.is_some() {
-            expr = Self::CompileTime(Box::new(expr));
-        }
+        let mut parser =
+            permutation((parser, opt(preceded(tag("..."), Self::_primary)))).map(|(begin, end)| {
+                if end.is_some() {
+                    Self::Ranged(Box::new(begin), Box::new(end.unwrap()))
+                } else {
+                    begin
+                }
+            });
 
-        let a = basic::symbol('{')(input);
-
-        let (input, expr) = if a.is_ok() {
-            let (input, expr2) =
-                delimited(basic::symbol('{'), Self::read, basic::symbol('}'))(input)?;
-            (input, Primary::Subscript(Box::new(expr), Box::new(expr2)))
-        } else {
-            (input, expr)
-        };
+        let (input, expr) = parser.parse(input)?;
 
         let (input, expr) = if input.starts_with("...") {
             let (input, _) = tag("...")(input)?;
@@ -219,40 +228,45 @@ impl Primary {
         Ok((input, expr))
     }
 
-    fn _read(input: &str) -> IResult<&str, Self> {
-        let a = alt((
-            apply_operator(&[BinaryOp::ShiftLeft, BinaryOp::ShiftRight]),
-            apply_operator(&[
-                BinaryOp::BitwiseAnd,
-                BinaryOp::BitwiseOr,
-                BinaryOp::BitwiseXor,
-            ]),
-            apply_operator(&[BinaryOp::Power]),
-            apply_operator(&[BinaryOp::Mul, BinaryOp::Div, BinaryOp::Mod]),
-            apply_operator(&[BinaryOp::Add, BinaryOp::Sub]),
-            apply_operator(&[
-                BinaryOp::LessThan,
-                BinaryOp::LessThanOrEqual,
-                BinaryOp::GreaterThan,
-                BinaryOp::GreaterThanOrEqual,
-            ]),
-            apply_operator(&[BinaryOp::Equal, BinaryOp::NotEqual]),
-            apply_operator(&[BinaryOp::LogicalAnd]),
-            apply_operator(&[BinaryOp::LogicalOr]),
-            Self::primary,
-        ))(input);
-
-        a
+    fn _pow(input: &str) -> IResult<&str, Self> {
+        Self::_binary_op(input, tag("**"), Self::_primary)
     }
-
-    // Parses _read = _read, _read
+    fn _level_shift(input: &str) -> IResult<&str, Self> {
+        Self::_binary_op(input, alt((tag("<<"), tag(">>"))), Self::_pow)
+    }
+    fn _bitwise(input: &str) -> IResult<&str, Self> {
+        Self::_binary_op(
+            input,
+            alt((tag("|"), tag("&"), tag("^"))),
+            Self::_level_shift,
+        )
+    }
+    fn _term(input: &str) -> IResult<&str, Self> {
+        Self::_binary_op(input, alt((tag("*"), tag("/"), tag("%"))), Self::_bitwise)
+    }
+    fn _math_expr(input: &str) -> IResult<&str, Self> {
+        Self::_binary_op(input, alt((tag("+"), tag("-"))), Self::_term)
+    }
+    fn _size_comp(input: &str) -> IResult<&str, Self> {
+        Self::_binary_op(
+            input,
+            alt((tag("<"), tag(">"), tag("<="), tag(">="))),
+            Self::_math_expr,
+        )
+    }
+    fn _equ_neq(input: &str) -> IResult<&str, Self> {
+        Self::_binary_op(input, alt((tag("!="), tag("=="))), Self::_size_comp)
+    }
+    fn _logical_and(input: &str) -> IResult<&str, Self> {
+        Self::_binary_op(input, tag("&&"), Self::_equ_neq)
+    }
+    fn _logical_or(input: &str) -> IResult<&str, Self> {
+        Self::_binary_op(input, tag("||"), Self::_logical_and)
+    }
+    fn _assignment(input: &str) -> IResult<&str, Self> {
+        Self::_binary_op(input, tag("="), Self::_logical_or)
+    }
     pub fn read(input: &str) -> IResult<&str, Self> {
-        let a = alt((
-            permutation((Self::_read, basic::symbol('='), Self::_read))
-                .map(|(l, _, r)| Self::Assign(Box::new(l), Box::new(r))),
-            Self::_read,
-        ))(input);
-
-        a
+        Self::_assignment(input)
     }
 }
