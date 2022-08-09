3d5acbd3d1be7e3de4c6669b076af20d8f76d753
syoch <hacksyoch@gmail.com>
2022-08-09 18:38:20 +0900

    Update: Change ident reader specifion

--- src/parser/expr/ident.rs
+++ src/parser/expr/ident.rs
@@ -1,12 +1,9 @@
-use nom::{character::complete::char, multi::separated_list1, IResult, Parser};
+use nom::{IResult, Parser};
 
 use crate::{ast::Expr, parser::basic::ident};
 
 pub fn _ident(input: &str) -> IResult<&str, Expr> {
-    separated_list1(char('.'), ident)
-        .map(|x| x.join("."))
-        .map(Expr::Ident)
-        .parse(input)
+    ident.map(Expr::Ident).parse(input)
 }
 
 #[cfg(test)]
@@ -29,18 +26,6 @@ mod test {
 
     #[test]
     fn test_ident_with_single_dot() {
-        assert_eq!(_ident("a.b"), Ok(("", Expr::Ident("a.b".to_string()))));
-    }
-
-    #[test]
-    fn test_ident_with_double_dot() {
-        assert_eq!(_ident("a.b.c"), Ok(("", Expr::Ident("a.b.c".to_string()))));
-    }
-    #[test]
-    fn test_ident_with_triple_dot() {
-        assert_eq!(
-            _ident("a.b.c.d"),
-            Ok(("", Expr::Ident("a.b.c.d".to_string())))
-        );
+        assert_eq!(_ident("a.b"), Ok((".b", Expr::Ident("a".to_string()))));
     }
 }
