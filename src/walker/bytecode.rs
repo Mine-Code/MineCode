4b6a8e5156589d6ee0d67062ed4db5010f5ddbc2
syoch <hacksyoch@gmail.com>
2022-08-13 22:05:34 +0900

    Update: Added fake implementation of ByteCoedWalker

--- src/walker/bytecode.rs
+++ src/walker/bytecode.rs
@@ -1,4 +1,4 @@
-use crate::ast::Expr;
+use crate::ast::{BinaryOp, Expr};
 
 use super::core_trait::Walker;
 
@@ -6,7 +6,7 @@ pub struct ByteCodeWalker {}
 
 impl Walker<Vec<u8>, Vec<u8>> for ByteCodeWalker {
     fn walk_load_module(module_name: String) -> Vec<u8> {
-        let ret = vec![0u8];
+        let mut ret = vec![0u8];
         ret.extend(module_name.as_bytes().to_vec());
         ret
     }
@@ -36,5 +36,57 @@ impl Walker<Vec<u8>, Vec<u8>> for ByteCodeWalker {
             } => Self::walk_for(name, *iter, *body, value.map(|x| *x)),
             Expr::Exprs(x) => Self::walk_exprs(x),
         }
+    }
+
+    fn walk_num(num: i32) -> Vec<u8> {
+        unimplemented!()
+    }
+    fn walk_ident(ident: String) -> Vec<u8> {
+        unimplemented!()
+    }
+    fn walk_string(string: String) -> Vec<u8> {
+        unimplemented!()
+    }
+    fn walk_func_call(func_name: Expr, args: Vec<Expr>) -> Vec<u8> {
+        unimplemented!()
+    }
+    fn walk_ranged(start: Expr, end: Expr) -> Vec<u8> {
+        unimplemented!()
+    }
+    fn walk_pointer(expr: Expr) -> Vec<u8> {
+        unimplemented!()
+    }
+    fn walk_compile_time(expr: Expr) -> Vec<u8> {
+        unimplemented!()
+    }
+    fn walk_apply_operator(op: BinaryOp, left: Expr, right: Expr) -> Vec<u8> {
+        unimplemented!()
+    }
+    fn walk_logical_not(expr: Expr) -> Vec<u8> {
+        unimplemented!()
+    }
+    fn walk_bitwise_not(expr: Expr) -> Vec<u8> {
+        unimplemented!()
+    }
+    fn walk_negative(expr: Expr) -> Vec<u8> {
+        unimplemented!()
+    }
+    fn walk_subscript(expr: Expr, index: Expr) -> Vec<u8> {
+        unimplemented!()
+    }
+    fn walk_attribute(expr: Expr, attr: String) -> Vec<u8> {
+        unimplemented!()
+    }
+    fn walk_if(branches: Vec<(Expr, Expr)>, fallback: Option<Expr>) -> Vec<u8> {
+        unimplemented!()
+    }
+    fn walk_for(name: String, iter: Expr, body: Expr, value: Option<Expr>) -> Vec<u8> {
+        unimplemented!()
+    }
+    fn walk_exprs(exprs: Vec<Expr>) -> Vec<u8> {
+        unimplemented!()
+    }
+    fn walk_func_def(name: String, args: Vec<String>, body: Expr) -> Vec<u8> {
+        unimplemented!()
     }
 }
