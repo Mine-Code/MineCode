db7b3aa0847fc5b45225884b26a3b9112bdda003
syoch <hacksyoch@gmail.com>
2022-08-13 22:27:00 +0900

    Update: Impl Into<u8>

--- src/ast/binaryop.rs
+++ src/ast/binaryop.rs
@@ -43,3 +43,30 @@ pub enum BinaryOp {
     #[strum(serialize = "=")]
     Assignment,
 }
+
+impl Into<u8> for BinaryOp {
+    fn into(self) -> u8 {
+        match self {
+            BinaryOp::Add => 0,
+            BinaryOp::Sub => 1,
+            BinaryOp::Mul => 2,
+            BinaryOp::Div => 3,
+            BinaryOp::Mod => 4,
+            BinaryOp::Power => 5,
+            BinaryOp::LogicalOr => 6,
+            BinaryOp::LogicalAnd => 7,
+            BinaryOp::BitwiseOr => 8,
+            BinaryOp::BitwiseAnd => 9,
+            BinaryOp::BitwiseXor => 10,
+            BinaryOp::ShiftLeft => 11,
+            BinaryOp::ShiftRight => 12,
+            BinaryOp::Equal => 13,
+            BinaryOp::NotEqual => 14,
+            BinaryOp::LessThan => 15,
+            BinaryOp::LessThanOrEqual => 16,
+            BinaryOp::GreaterThan => 17,
+            BinaryOp::GreaterThanOrEqual => 18,
+            BinaryOp::Assignment => 19,
+        }
+    }
+}
