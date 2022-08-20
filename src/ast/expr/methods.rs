0e3d699599128c4e989e2e659ae83b127d4c66d6
syoch <hacksyoch@gmail.com>
2022-08-20 15:50:12 +0900

    Update: write soem test

--- src/ast/expr/methods.rs
+++ src/ast/expr/methods.rs
@@ -22,4 +22,32 @@ impl Expr {
     }
 }
 
-// TODO: Write test
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    #[test]
+    fn test_multiple_expr_coff_0() {
+        let expr = Expr::multiply(0, Expr::Num(1));
+        assert_eq!(expr, Expr::Num(0));
+    }
+
+    #[test]
+    fn test_multiple_expr_coff_1() {
+        let expr = Expr::multiply(1, Expr::Ident("a".to_string()));
+        assert_eq!(expr, Expr::Ident("a".to_string()));
+    }
+
+    #[test]
+    fn test_multiple_expr_coff_2() {
+        let expr = Expr::multiply(2, Expr::Ident("a".to_string()));
+        assert_eq!(
+            expr,
+            Expr::ApplyOperator(
+                BinaryOp::Mul,
+                Box::new(Expr::Num(2)),
+                Box::new(Expr::Ident("a".to_string())),
+            )
+        );
+    }
+}
